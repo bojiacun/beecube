dbb636f894824cc5199954a854c89103e72bb15f
18612365082 <bojinhong@126.com>
2022-10-30 13:44:34 +0800

    框架初始化设置

--- adminui/app/routes/index.tsx
+++ adminui/app/routes/index.tsx
@@ -1,7 +1,10 @@
+import {Button} from "react-bootstrap";
+
 export default function Index() {
     return (
         <div>
             <h1>Welcome to Remix 12344567890</h1>
+            <Button variant={'danger'}>测试按钮</Button>
         </div>
     );
 }
