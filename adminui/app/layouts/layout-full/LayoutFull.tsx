5512215c5685bb9b4cd201ff8f0c7f5016fa0e04
18612365082 <bojinhong@126.com>
2022-10-30 17:55:07 +0800

    框架初始化设置

--- adminui/app/layouts/layout-full/LayoutFull.tsx
+++ adminui/app/layouts/layout-full/LayoutFull.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useContext} from "react";
+import ThemeContext from 'themeConfig';
 
 
 const LayoutFull: React.FC = (props:any) => {
+    const themeContext = useContext(ThemeContext);
+
     const {children} = props;
     return (
         <div>
