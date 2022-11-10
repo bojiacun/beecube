436745df635e27a8d2ee1eb60ce190e5e6614307
18612365082 <bojinhong@126.com>
2022-11-10 13:37:51 +0800

    框架初始化设置

--- adminui/app/routes/index.tsx
+++ adminui/app/routes/index.tsx
@@ -1,7 +1,8 @@
 import {LoaderFunction, redirect} from "@remix-run/node";
+import {LOGIN_SUCCESS_URL} from "~/utils/request.server";
 
 export const loader: LoaderFunction = async () => {
-    return redirect('/test');
+    return redirect(LOGIN_SUCCESS_URL);
 }
 
 export default function Index() {
