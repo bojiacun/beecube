df5b93efa9e4e5ce6fee406337241b6ed111c8d0
18612365082 <bojinhong@126.com>
2022-11-10 09:35:30 +0800

    框架初始化设置

--- adminui/app/routes/captcha[.png].tsx
+++ adminui/app/routes/captcha[.png].tsx
@@ -2,8 +2,9 @@ import {LoaderFunction} from "@remix-run/node";
 import {API_CAPTCHA} from "~/utils/request.server";
 
 
-export const loader: LoaderFunction = async ({params}) => {
-    const _t = params['_t'];
+export const loader: LoaderFunction = async ({request}) => {
+    const url = new URL(request.url);
+    const _t = url.searchParams.get('_t');
     //获取图片数据
     const remoteCaptchaImageUrl = API_CAPTCHA + '/' + _t;
     const res = await fetch(remoteCaptchaImageUrl);
