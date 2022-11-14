c86a015afad2afc3d1cd133e972d730649573606
18612365082 <bojinhong@126.com>
2022-11-14 22:07:41 +0800

    框架初始化设置

--- adminui/app/pages/system/roles/TreePermissionList.tsx
+++ adminui/app/pages/system/roles/TreePermissionList.tsx
@@ -1,10 +1,9 @@
 import {Modal} from "react-bootstrap";
 import {useEffect, useState} from "react";
 import {useFetcher} from "@remix-run/react";
-import {ChevronDown, ChevronRight} from "react-feather";
 import CheckboxTree from 'react-checkbox-tree';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCheckSquare} from '@fortawesome/free-regular-svg-icons';
+import {AwesomeButton} from "react-awesome-button";
 
 const nodes = [{
     value: 'mars',
@@ -17,6 +16,8 @@ const nodes = [{
 const TreePermissionList = (props:any) => {
     const {model, setAuthModel} = props;
     const [treeData, setTreeData] = useState<any>();
+    const [checked, setChecked] = useState<any[]>([]);
+    const [expanded, setExpanded] = useState<any[]>([]);
     const searchFetcher = useFetcher();
 
     useEffect(()=>{
@@ -43,14 +44,21 @@ const TreePermissionList = (props:any) => {
             <Modal.Header closeButton>
                 <Modal.Title id={'role-tree-permission'}>{model?.roleName}授权</Modal.Title>
             </Modal.Header>
+            <Modal.Body>
+                <div className={'mb-1'}>所拥有权限：</div>
             <CheckboxTree
                 nodes={nodes}
+                checked={checked}
+                expanded={expanded}
+                onCheck={checked1 => setChecked(checked1)}
+                onExpand={expanded1 => setExpanded(expanded1)}
+                iconsClass={'fa6'}
                 icons={{
-                    check: <FontAwesomeIcon className="rct-icon rct-icon-check" icon={faCheckSquare} />,
-                    uncheck: <FontAwesomeIcon className="rct-icon rct-icon-uncheck" icon={['fas', 'square']} />,
+                    check: <FontAwesomeIcon className="rct-icon rct-icon-check" icon={'check-square'} />,
+                    uncheck: <FontAwesomeIcon className="rct-icon rct-icon-uncheck" icon={['far', 'square']} />,
                     halfCheck: <FontAwesomeIcon className="rct-icon rct-icon-half-check" icon="check-square" />,
-                    expandClose: <FontAwesomeIcon className="rct-icon rct-icon-half-check" icon={faCheckSquare} />,
-                    expandOpen: <FontAwesomeIcon className="rct-icon rct-icon-expand-open" icon="chevron-down" />,
+                    expandClose: <FontAwesomeIcon className="rct-icon rct-icon-half-check" icon={'caret-right'} />,
+                    expandOpen: <FontAwesomeIcon className="rct-icon rct-icon-expand-open" icon="caret-down" />,
                     expandAll: <FontAwesomeIcon className="rct-icon rct-icon-expand-all" icon="plus-square" />,
                     collapseAll: <FontAwesomeIcon className="rct-icon rct-icon-collapse-all" icon="minus-square" />,
                     parentClose: <FontAwesomeIcon className="rct-icon rct-icon-parent-close" icon="folder" />,
@@ -58,6 +66,16 @@ const TreePermissionList = (props:any) => {
                     leaf: <FontAwesomeIcon className="rct-icon rct-icon-leaf-close" icon="file" />
                 }}
             />
+            </Modal.Body>
+            <Modal.Footer>
+                <AwesomeButton
+                    key={'submit'}
+                    type={'primary'}
+                    containerProps={{type: 'submit'}}
+                >
+                    保存
+                </AwesomeButton>
+            </Modal.Footer>
         </Modal>
     );
 }
