2b3d1495b2b0d3795934c594e092bceaadba646d
18612365082 <bojinhong@126.com>
2022-11-14 22:29:57 +0800

    框架初始化设置

--- adminui/app/pages/system/roles/TreePermissionList.tsx
+++ adminui/app/pages/system/roles/TreePermissionList.tsx
@@ -9,25 +9,25 @@ const nodes = [{
     value: 'mars',
     label: 'Mars',
     children: [
-        { value: 'phobos', label: 'Phobos' },
-        { value: 'deimos', label: 'Deimos' },
+        {value: 'phobos', label: 'Phobos'},
+        {value: 'deimos', label: 'Deimos'},
     ],
 }];
-const TreePermissionList = (props:any) => {
+const TreePermissionList = (props: any) => {
     const {model, setAuthModel} = props;
     const [treeData, setTreeData] = useState<any>();
     const [checked, setChecked] = useState<any[]>([]);
     const [expanded, setExpanded] = useState<any[]>([]);
     const searchFetcher = useFetcher();
 
-    useEffect(()=>{
-        if(model) {
+    useEffect(() => {
+        if (model) {
             searchFetcher.load('/system/roles/tree');
         }
     }, [model]);
 
-    useEffect(()=>{
-        if(searchFetcher.type === 'done' && searchFetcher.data) {
+    useEffect(() => {
+        if (searchFetcher.type === 'done' && searchFetcher.data) {
             setTreeData(searchFetcher.data.treeList);
         }
     }, [searchFetcher.state]);
@@ -46,26 +46,26 @@ const TreePermissionList = (props:any) => {
             </Modal.Header>
             <Modal.Body>
                 <div className={'mb-1'}>所拥有权限：</div>
-            <CheckboxTree
-                nodes={nodes}
-                checked={checked}
-                expanded={expanded}
-                onCheck={checked1 => setChecked(checked1)}
-                onExpand={expanded1 => setExpanded(expanded1)}
-                iconsClass={'fa6'}
-                icons={{
-                    check: <FontAwesomeIcon className="rct-icon rct-icon-check" icon={'check-square'} />,
-                    uncheck: <FontAwesomeIcon className="rct-icon rct-icon-uncheck" icon={['far', 'square']} />,
-                    halfCheck: <FontAwesomeIcon className="rct-icon rct-icon-half-check" icon="check-square" />,
-                    expandClose: <FontAwesomeIcon className="rct-icon rct-icon-half-check" icon={'caret-right'} />,
-                    expandOpen: <FontAwesomeIcon className="rct-icon rct-icon-expand-open" icon="caret-down" />,
-                    expandAll: <FontAwesomeIcon className="rct-icon rct-icon-expand-all" icon="plus-square" />,
-                    collapseAll: <FontAwesomeIcon className="rct-icon rct-icon-collapse-all" icon="minus-square" />,
-                    parentClose: <FontAwesomeIcon className="rct-icon rct-icon-parent-close" icon="folder" />,
-                    parentOpen: <FontAwesomeIcon className="rct-icon rct-icon-parent-open" icon="folder-open" />,
-                    leaf: <FontAwesomeIcon className="rct-icon rct-icon-leaf-close" icon="file" />
-                }}
-            />
+                <CheckboxTree
+                    nodes={nodes}
+                    checked={checked}
+                    expanded={expanded}
+                    onCheck={checked1 => setChecked(checked1)}
+                    onExpand={expanded1 => setExpanded(expanded1)}
+                    iconsClass={'fa6'}
+                    icons={{
+                        check: <FontAwesomeIcon className="rct-icon rct-icon-check" icon={'check-square'}/>,
+                        uncheck: <FontAwesomeIcon className="rct-icon rct-icon-uncheck" icon={['far', 'square']}/>,
+                        halfCheck: <FontAwesomeIcon className="rct-icon rct-icon-half-check" icon="check-square"/>,
+                        expandClose: <FontAwesomeIcon className="rct-icon rct-icon-half-check" icon={'caret-right'}/>,
+                        expandOpen: <FontAwesomeIcon className="rct-icon rct-icon-expand-open" icon="caret-down"/>,
+                        expandAll: <FontAwesomeIcon className="rct-icon rct-icon-expand-all" icon="plus-square"/>,
+                        collapseAll: <FontAwesomeIcon className="rct-icon rct-icon-collapse-all" icon="minus-square"/>,
+                        parentClose: <FontAwesomeIcon className="rct-icon rct-icon-parent-close" icon="folder"/>,
+                        parentOpen: <FontAwesomeIcon className="rct-icon rct-icon-parent-open" icon="folder-open"/>,
+                        leaf: <FontAwesomeIcon className="rct-icon rct-icon-leaf-close" icon="file"/>
+                    }}
+                />
             </Modal.Body>
             <Modal.Footer>
                 <AwesomeButton
