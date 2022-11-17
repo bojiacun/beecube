a21fde7835ee1924ecfc6ac7cba91cf9cb121ebc
18612365082 <bojinhong@126.com>
2022-11-17 08:40:36 +0800

    框架初始化设置

---
+++ adminui/app/components/filebrowser/form.tsx
@@ -0,0 +1,185 @@
+import type { FC} from "react";
+import React, { useState } from "react";
+import FileBrowser, { FILE_TYPE_AUDIO, FILE_TYPE_IMAGE, FILE_TYPE_OTHER, FILE_TYPE_VIDEO } from "./index";
+import {Button, FormControl, Image, Row, Col, InputGroup, Modal} from "react-bootstrap";
+import {DownloadCloud, XCircle} from "react-feather";
+import FallbackImage from "~/components/fallback-image";
+
+
+interface FileBrowserInputProps {
+    type: number;
+    multi?: boolean;
+    browserWidth?: number;
+    onChange?: (value: any) => void;
+    value?: string;
+    imagePreview?: boolean;
+    previewWidth?: number;
+    previewHeight?: number;
+}
+
+
+function resolveUrl(path: string) {
+    return '/' + path;
+}
+
+
+const FileBrowserInput: FC<FileBrowserInputProps> = React.forwardRef<any, FileBrowserInputProps>((props, ref) => {
+    const { type, multi=false, browserWidth, onChange, value, imagePreview=false, previewHeight=80, previewWidth=80, ...rest } = props;
+    const [modalVisible, setModalVisible] = useState<boolean>(false);
+    const [selectedFiles, setSelectedFiles] = useState<any[]>([]);
+    let buttonText: string = '';
+    switch (type) {
+        case FILE_TYPE_IMAGE:
+            buttonText = '选择图片';
+            break;
+        case FILE_TYPE_AUDIO:
+            buttonText = '选择音频';
+            break;
+        case FILE_TYPE_VIDEO:
+            buttonText = '选择视频';
+            break;
+        case FILE_TYPE_OTHER:
+            buttonText = '选择文件';
+            break;
+    }
+    const doSelect = () => {
+        if (selectedFiles && selectedFiles.length > 0) {
+            typeof onChange === 'function' && onChange(selectedFiles.map(item => item.attachment).join(','));
+        }
+        setModalVisible(false);
+    }
+    const renderFooter = () => {
+        return (
+            <>
+                <Button onClick={() => setModalVisible(false)}>取消</Button>
+                <Button type="primary" onClick={() => doSelect()}>
+                    确定选择
+                </Button>
+            </>
+        );
+    };
+    const handleFileChanged = (values: any[]) => {
+        setSelectedFiles(values);
+    }
+    const handleFileDelete = (val:any) => {
+        console.log(val);
+    }
+    const handleInputValueChanged = (val:any) => {
+        typeof onChange === 'function' && onChange(val);
+    }
+    const removeFile = (v:string) => {
+        let values = value?.split(',');
+        values = values?.filter(val => val != v);
+        if(onChange) {
+            onChange(values?.join(','));
+        }
+    }
+    return (
+        <div className={'filebrowser'}>
+            {!imagePreview &&
+                <InputGroup>
+                    <FormControl ref={ref} readOnly={multi} onChange={handleInputValueChanged} value={multi?'':value} {...rest}  />
+                    <InputGroup.Append>
+                        <Button onClick={() => setModalVisible(true)}>浏览</Button>
+                    </InputGroup.Append>
+                </InputGroup>
+            }
+            {imagePreview && 
+                <Row style={{marginTop: 0, minWidth: 60}}>
+                    <Col className={'previewItem'}>
+                        <XCircle size={16} className={'close'} onClick={()=>removeFile(value!)} />
+                        <FallbackImage src={resolveUrl(value!)} style={{width:previewWidth, height: previewHeight, cursor: 'pointer'}} onClick={()=>setModalVisible(true)} />
+                    </Col>
+                </Row>
+            }
+            {type === FILE_TYPE_IMAGE && !imagePreview && value && value.split(',').length > 0 &&
+                <Row style={{marginTop: 10}}>
+                    {
+                        value.split(',').map(v => {
+                            return (
+                                <Col className={'previewItem'} key={v}>
+                                    <XCircle className={'close'} onClick={()=>removeFile(v)} />
+                                    <Image width={160} src={resolveUrl(v)} className={'preview'} />
+                                </Col>
+                            );
+                        })
+                    }
+                </Row>
+            }
+            {type === FILE_TYPE_VIDEO && value && value.split(',').length > 0 &&
+                <Row style={{marginTop: 10}}>
+                    {
+                        value.split(',').map(v => {
+                            return (
+                                <Col className={'previewItem'} key={v}>
+                                    <XCircle className={'close'} onClick={()=>removeFile(v)} />
+                                    <video src={resolveUrl(v)} controls={true} style={{width: 320}} className={'previeew'}> 您的浏览器不支持 video 标签。 </video>
+                                </Col>
+                            );
+                        })
+                    }
+                </Row>
+            }
+            {type === FILE_TYPE_AUDIO && value && value.split(',').length > 0 &&
+                <Row style={{marginTop: 10}}>
+                    {
+                        value.split(',').map(v => {
+                            return (
+                                <Col className={'previewItem'} key={v}>
+                                    <XCircle className={'close'} onClick={()=>removeFile(v)} />
+                                    <audio src={resolveUrl(v)} controls={true} style={{width: 320}} className={'preview'}> 您的浏览器不支持 audio 标签。 </audio>
+                                </Col>
+                            );
+                        })
+                    }
+                </Row>
+            }
+            {type === FILE_TYPE_OTHER && value && value.split(',').length > 0 &&
+                <Row style={{marginTop: 10}}>
+                    {
+                        value.split(',').map(v => {
+                            return (
+                                <Col className={'previewItem'} key={v}>
+                                    <XCircle className={'close'} onClick={()=>removeFile(v)} />
+                                    <div className={'preview'} style={{width: 160, height: 160, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                                        <a href={resolveUrl(v)} target="_blank"><DownloadCloud style={{fontSize: 80}} /></a>
+                                    </div>
+                                </Col>
+                            );
+                        })
+                    }
+                </Row>
+            }
+            <Modal
+                bodyStyle={{ padding: '0 20px' }}
+                destroyOnClose
+                width={browserWidth}
+                show={modalVisible}
+                onHide={() => setModalVisible(false)}
+            >
+                <Modal.Title>
+                    {buttonText}
+                </Modal.Title>
+                <FileBrowser
+                    request={(page, type) => []}
+                    onChange={handleFileChanged}
+                    type={type}
+                    uploadUrl={'/'}
+                    onDelete={handleFileDelete}
+                    multi={multi}
+                />
+                <Modal.Footer>
+                    {renderFooter()}
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+
+});
+
+FileBrowserInput.defaultProps = {
+    multi: false,
+    browserWidth: 1000,
+}
+
+export default FileBrowserInput
---
+++ adminui/app/components/filebrowser/index.tsx
@@ -0,0 +1,259 @@
+import {FC, useEffect, useState} from "react";
+// @ts-ignore
+import plupload from "plupload";
+// @ts-ignore
+import moxieSwf from 'assets/Moxie.swf';
+// @ts-ignore
+import moxieXap from 'assets/Moxie.xap';
+import {ListGroup, Row, Col, Button, Form, ProgressBar} from "react-bootstrap";
+import {CheckCircle, FileText, Speaker, Video} from "react-feather";
+
+export const FILE_TYPE_IMAGE = 1
+export const FILE_TYPE_AUDIO = 2
+export const FILE_TYPE_VIDEO = 3
+export const FILE_TYPE_OTHER = 4
+
+function resolveUrl(path: string) {
+    return '/' + path;
+}
+
+
+interface FileBrowserProps {
+    request: (page: number, type: number) => void;
+    onChange: (values: any[]) => void;
+    type: number;
+    uploadUrl: string;
+    onDelete: (value: any) => void;
+    multi: boolean;
+}
+
+const FileBrowser: FC<FileBrowserProps> = (props) => {
+    const [items, setItems] = useState<any[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [deleting, setDeleting] = useState<boolean>(false);
+    const [page, setPage] = useState({totalElements: 0, size: 20, number: 1});
+    const [canDelete, setCanDelete] = useState(false);
+    const [progress, setProgress] = useState({percent: 0});
+    const {request, onChange, type = FILE_TYPE_IMAGE, onDelete, uploadUrl, multi} = props;
+    let filters: any;
+    switch (type) {
+        case FILE_TYPE_IMAGE:
+            filters = {
+                name: '图片',
+                max_file_size: "20mb",
+                mime_types: [
+                    {
+                        title: "图片文件",
+                        extensions: "jpg,gif,png,jpeg,webp"
+                    }
+                ]
+            };
+            break;
+        case FILE_TYPE_AUDIO:
+            filters = {
+                name: '音频',
+                max_file_size: "500mb",
+                mime_types: [
+                    {
+                        title: "Audio files",
+                        extensions: "mp3"
+                    }
+                ]
+            };
+            break;
+        case FILE_TYPE_VIDEO:
+            filters = {
+                name: '视频',
+                max_file_size: "3000mb",
+                mime_types: [
+                    {
+                        title: "Video files",
+                        extensions: "mp4,wmv,rmvb,avi"
+                    }
+                ]
+            };
+            break;
+        case FILE_TYPE_OTHER:
+            filters = {
+                name: '文件',
+                max_file_size: "1000mb",
+                mime_types: [
+                    {
+                        title: "files",
+                        extensions: "*"
+                    }
+                ]
+            };
+            break;
+        default:
+            filters = {
+                max_file_size: "0mb"
+            }
+            break;
+    }
+
+    const checkItem = (item: any) => {
+        console.log('multi', multi);
+        if (!multi) {
+            //取消所有所选
+            items.map((itm: any) => itm.checked = false);
+        }
+        item.checked = !item.checked;
+        setItems([...items]);
+        setCanDelete(items.filter((itm: any) => itm.checked === true).length > 0);
+        typeof onChange === 'function' && onChange(items.filter((itm: any) => itm.checked === true));
+    }
+    const checkAll = (e:any) => {
+        setCanDelete(e.target.checked);
+        setItems(items.map(item => {
+            item.checked = e.target.checked;
+            return item
+        }))
+        typeof onChange === 'function' && onChange(items.filter(itm => itm.checked === true));
+    }
+    const loadData = (page = 1) => {
+        setLoading(true);
+        setCanDelete(false);
+        // typeof request === 'function' && request(page, type).then(res => {
+        //     setLoading(false);
+        //     res.data.number++;
+        //     setPage(res.data);
+        //     setItems(res.data.content);
+        // }).catch(e => setLoading(false));
+    }
+    const doDelete = () => {
+        if (typeof onDelete === 'function') {
+            const itemsToDelete = items.filter(item => item.checked === true);
+            const requests = [];
+            setDeleting(true);
+
+            for (let item of itemsToDelete) {
+                requests.push(onDelete(item));
+            }
+
+            Promise.all(requests).then(() => {
+                loadData(page.number);
+                setDeleting(false);
+            });
+        }
+    }
+    const pageSizeChanged = (newPage: number, pageSize: number) => {
+        loadData(newPage);
+    }
+    useEffect(() => {
+        const uploader = new plupload.Uploader({
+            runtimes: "html5,flash,silverlight,html4",
+            browse_button: document.getElementById('browseBtn'),
+            url: uploadUrl,
+            flash_swf_url: moxieSwf,
+            silverlight_xap_url: moxieXap,
+            chunk_size: '1m',
+            multi_selection: true,
+            multipart_params: {
+                type: type
+            },
+            headers: {},
+            filters,
+            init: {
+                PostInit: function () {
+                },
+
+                FilesAdded: function (up: any, files: any) {
+                    up.start();
+                },
+
+                UploadComplete: function (up: any, files: any) {
+                },
+
+                FileUploaded: function (up: any, files: any) {
+                    loadData(page.number);
+                },
+
+                UploadProgress: function (up: any, file: any) {
+                    setProgress(() => ({...file}));
+                },
+
+                Error: function (up: any, err: any) {
+                    setProgress(() => ({...err.file}));
+                }
+            }
+        })
+        uploader.init();
+        loadData();
+    }, []);
+    let renderChildren:any;
+    switch (type) {
+        case FILE_TYPE_IMAGE:
+            renderChildren = (item: any) => (
+                <ListGroup.Item className={'item'} style={{backgroundImage: 'url(' + resolveUrl(item.attachment) + ')'}}
+                           onClick={() => checkItem(item)}>
+                    {item.checked && <div className={'mask'}><CheckCircle size={16} className={'icon'}/></div>}
+                    <div className={'name'}>{item.filename}</div>
+                </ListGroup.Item>
+            )
+            break;
+        case FILE_TYPE_AUDIO:
+            renderChildren = (item: any) => (
+                <ListGroup.Item className={'item'} onClick={() => checkItem(item)}>
+                    <Speaker className={'audio'}/>
+                    {item.checked && <div className={'mask'}><CheckCircle className={'icon'}/></div>}
+                    <div className={'name'}>{item.filename}</div>
+                </ListGroup.Item>
+            )
+            break;
+        case FILE_TYPE_VIDEO:
+            renderChildren = (item: any) => (
+                <ListGroup.Item className={'item'} onClick={() => checkItem(item)}>
+                    <Video className={'video'}/>
+                    {item.checked && <div className={'mask'}><CheckCircle className={'icon'}/></div>}
+                    <div className={'name'}>{item.filename}</div>
+                </ListGroup.Item>
+            )
+            break;
+        case FILE_TYPE_OTHER:
+            renderChildren = (item: any) => (
+                <ListGroup.Item className={'item'} onClick={() => checkItem(item)}>
+                    <FileText className={'excel'}/>
+                    {item.checked && <div className={'mask'}><CheckCircle className={'icon'}/></div>}
+                    <div className={'name'}>{item.filename}</div>
+                </ListGroup.Item>
+            )
+            break;
+        default:
+            renderChildren = <></>
+            break;
+    }
+    return (
+        <div className={'filebrowser'}>
+            <div className={'container'}>
+                <Row className={'header'}>
+                    <Col>
+                        {progress.percent > 0 && <ProgressBar />}
+                    </Col>
+                    <Col>
+                        <Button disabled={!canDelete || deleting} onClick={doDelete}>删除</Button>
+                        <Button type="primary" id="browseBtn">上传{filters.name}</Button>
+                    </Col>
+                </Row>
+                <ListGroup>
+                    {items.map((item:any)=>{
+                        return renderChildren(item);
+                    })}
+                </ListGroup>
+                <Row className={'footer'}>
+                    <Col>
+                        {multi && <Form.Check onChange={checkAll}>全选</Form.Check>}
+                    </Col>
+                    <Col style={{textAlign: 'right'}}>
+                    </Col>
+                </Row>
+            </div>
+        </div>
+    );
+}
+
+FileBrowser.defaultProps = {
+    multi: false
+}
+
+export default FileBrowser
