6a7985491f9de6b015a503a29717fa800939c6ee
18612365082 <bojinhong@126.com>
2022-11-24 22:04:45 +0800

    框架初始化设置

--- adminui/app/navigation/vertical/monitor.js
+++ adminui/app/navigation/vertical/monitor.js
@@ -4,6 +4,11 @@ export default [
         header: 'System Monitor Pages',
     },
     {
+        title: '网关路由',
+        route: 'monitor/gateway',
+        icon: 'GitMerge'
+    },
+    {
         title: '定时任务',
         route: 'monitor/crons',
         icon: 'Clock'
@@ -17,6 +22,12 @@ export default [
         title: 'system log',
         route: 'monitor/log',
         icon: 'Terminal'
+    },
+    {
+        title: 'SQL监控',
+        route: '/druid/login.html',
+        icon: 'Crosshair',
+        target: '_blank'
     },
     {
         title: '请求追踪',
--- adminui/app/routes/monitor/gateway.tsx
+++ adminui/app/routes/monitor/gateway.tsx
@@ -1,137 +1,162 @@
 import {
     Col,
     FormGroup,
-    Table,
     Card,
     InputGroup,
     Form,
     FormControl,
     FormLabel,
-    Button,
+    Button, Row, Dropdown, Badge,
 } from "react-bootstrap";
 import vueSelectStyleUrl from '~/styles/react/libs/vue-select.css';
 import {json, LinksFunction, LoaderFunction} from "@remix-run/node";
-import {API_GATEWAY_LIST, API_ROLE_LIST, requestWithToken} from "~/utils/request.server";
+import {API_CRONJOB_LIST, API_DATALOG_LIST, API_GATEWAY_LIST, requestWithToken} from "~/utils/request.server";
 import {useCatch, useFetcher, useLoaderData} from "@remix-run/react";
 import {withPageLoading} from "~/utils/components";
-import {useEffect, useState} from "react";
-import {DefaultListSearchParams, defaultRouteCatchBoundary, defaultRouteErrorBoundary} from "~/utils/utils";
+import SinglePagination from "~/components/pagination/SinglePagination";
+import {useContext, useEffect, useRef, useState} from "react";
+import {DefaultListSearchParams, defaultRouteCatchBoundary, defaultRouteErrorBoundary, PageSizeOptions, showDeleteAlert} from "~/utils/utils";
+import BootstrapTable from 'react-bootstrap-table-next';
 import _ from 'lodash';
 import querystring from 'querystring';
+import ReactSelectThemed from "~/components/react-select-themed/ReactSelectThemed";
+import {requireAuthenticated} from "~/utils/auth.server";
 import Error500Page from "~/components/error-page/500";
 import Error401Page from "~/components/error-page/401";
 import Error404Page from "~/components/error-page/404";
+import {Delete, Edit, MoreVertical} from "react-feather";
+import CronEdit from "~/pages/monitor/crons/CronEdit";
 
 export const links: LinksFunction = () => {
     return [{rel: 'stylesheet', href: vueSelectStyleUrl}];
 }
+
 export const ErrorBoundary = defaultRouteErrorBoundary;
 
 export const CatchBoundary = defaultRouteCatchBoundary;
 
+
 export const loader: LoaderFunction = async ({request}) => {
+    await requireAuthenticated(request);
     const url = new URL(request.url);
-    if(_.isEmpty(url.search)) {
-        url.search = '?'+querystring.stringify(DefaultListSearchParams);
+    let queryString = '';
+    if (_.isEmpty(url.search)) {
+        queryString = '?' + querystring.stringify(DefaultListSearchParams);
+    } else {
+        queryString = '?' + url.searchParams.toString();
     }
-    const result = await requestWithToken(request)(API_GATEWAY_LIST+url.search);
+    const result = await requestWithToken(request)(API_GATEWAY_LIST + queryString);
     return json(result.result);
 }
 
-const GatewayPages = () => {
-    const loaderData = useLoaderData();
-    const [records, setRecords] = useState(loaderData?.records || []);
-    const searchFetcher = useFetcher();
 
-    useEffect(()=>{
-        if(searchFetcher.data) {
-            setRecords(searchFetcher.data.records);
+const GatewayPages = () => {
+    const [list, setList] = useState<any>(useLoaderData());
+    const [editModel, setEditModel] = useState<any>();
+    const [searchState, setSearchState] = useState<any>(DefaultListSearchParams);
+    const searchFetcher = useFetcher();
+    const resumeFetcher = useFetcher();
+    const runJobFetcher = useFetcher();
+
+    useEffect(() => {
+        if (searchFetcher.data) {
+            setList(searchFetcher.data);
         }
     }, [searchFetcher.state]);
+
+
+    const loadData = () => {
+        searchFetcher.submit(searchState);
+    }
+
+    const handlePageChanged = (e: any) => {
+        searchState.pageNo = e.selected + 1;
+        setSearchState({...searchState});
+        searchFetcher.submit(searchState, {method: 'get'});
+    }
+    const handlePageSizeChanged = (newValue: any) => {
+        searchState.pageSize = parseInt(newValue.value);
+        setSearchState({...searchState});
+        searchFetcher.submit(searchState, {method: 'get'});
+    }
+    const handleOnAction = (row: any, e: any) => {
+        switch (e) {
+            case 'edit':
+                //编辑
+                break;
+            case 'delete':
+                //删除按钮
+                showDeleteAlert(function () {
+                });
+                break;
+        }
+    }
+    const columns: any[] = [
+        {
+            text: '路由ID',
+            dataField: 'routerId',
+            classes: 'text-cut',
+            headerStyle: {width: 400}
+        },
+        {
+            text: '路由名称',
+            dataField: 'name',
+            classes: 'text-cut',
+            headerStyle: {width: 200}
+        },
+        {
+            text: '路由URI',
+            dataField: 'uri',
+            classes: 'text-cut'
+        },
+        {
+            text: '状态',
+            dataField: 'status_dictText',
+            headerStyle: {width: 100},
+            formatter: (cell: number, row: any) => {
+                return row.status === 1 ? <Badge variant={'success'}>正常</Badge> :
+                    <Badge variant={'danger'}>异常</Badge>;
+            }
+        },
+        {
+            text: '操作',
+            dataField: 'operation',
+            isDummyField: true,
+            headerStyle: {width: 180},
+            formatter: (cell: any, row: any) => {
+                return (
+                    <div className={'d-flex align-items-center'}>
+                        <a href={'#'} onClick={() => handleOnAction(row, 'edit')}>编辑</a>
+                        <span className={'divider'}/>
+                        <a href={'#'} onClick={() => handleOnAction(row, 'delete')}>删除</a>
+                    </div>
+                );
+            }
+        },
+    ]
 
     return (
         <>
             <Card>
-                <Card.Header>
-                    <Card.Title>
-                        网关路由
-                    </Card.Title>
-                </Card.Header>
-                <Card.Body className={'d-flex justify-content-between  flex-wrap'}>
-                    <Form inline>
-                        <FormGroup as={Form.Row} className={'align-items-center mr-1 mb-md-0'}>
-                            <FormLabel column={'sm'}>排序</FormLabel>
-                            <InputGroup as={'span'} size={'sm'}>
-                                <FormControl as={'select'} size={'sm'}>
-                                    <option>无</option>
-                                </FormControl>
-                            </InputGroup>
-                            <InputGroup as={'span'} size={'sm'}>
-                                <FormControl as={'select'} size={'sm'}>
-                                    <option>升序</option>
-                                    <option>降序</option>
-                                </FormControl>
-                            </InputGroup>
-                        </FormGroup>
-                    </Form>
-                    <searchFetcher.Form className={'form-inline'}>
-                        <FormControl name={'pageNo'} value={DefaultListSearchParams.pageNo} type={'hidden'} />
-                        <FormControl name={'column'} value={DefaultListSearchParams.column} type={'hidden'} />
-                        <FormControl name={'order'} value={DefaultListSearchParams.order} type={'hidden'} />
-
-                        <FormGroup as={Form.Row} className={'mb-0'}>
-                            <FormLabel column={'sm'} sm={2} htmlFor={'roleName'}>筛选</FormLabel>
-                            <Col sm={10}>
-                                <InputGroup size={'sm'}>
-                                    <FormControl name={'roleName'} placeholder={'请输入要搜索的内容'}/>
-                                    <InputGroup.Append>
-                                        <Button type={'submit'}>搜索</Button>
-                                    </InputGroup.Append>
-                                </InputGroup>
-                            </Col>
-                        </FormGroup>
-                    </searchFetcher.Form>
-                </Card.Body>
-
-                <Table striped hover responsive className={'position-relative'}>
-                    <thead>
-                    <tr>
-                        <th>角色名称</th>
-                        <th>角色编码</th>
-                        <th>创建时间</th>
-                        <th>操作</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {records.map((item: any) => {
-                        return (
-                            <tr key={item.id}>
-                                <td>{item.roleName}</td>
-                                <td>{item.roleCode}</td>
-                                <td>{item.createTime}</td>
-                                <td></td>
-                            </tr>
-                        );
-                    })}
-                    </tbody>
-                </Table>
-
-                <Card.Body className={'d-flex justify-content-between flex-wrap pt-0 mt-1'}>
-                    <div className={'align-items-center mr-1 mb-md-0'}>
-                        <Form inline>
-                            <FormGroup as={Form.Row}>
-                                <FormLabel column={'sm'}>共计{loaderData?.total}条数据,每页显示</FormLabel>
-                                <InputGroup size={'sm'} as={'span'}>
-                                    <FormControl as={'select'} size={'sm'}>
-                                        <option>20</option>
-                                        <option>50</option>
-                                        <option>100</option>
-                                    </FormControl>
-                                </InputGroup>
-                            </FormGroup>
-                        </Form>
-                    </div>
-                </Card.Body>
+                <div className={'m-2'}>
+                    <Row>
+                        <Col md={6} className={'d-flex align-items-center justify-content-start mb-1 mb-md-0'}>
+                            <h4 className="mb-0">网关路由</h4>
+                            <ReactSelectThemed
+                                placeholder={'分页大小'}
+                                isSearchable={false}
+                                defaultValue={PageSizeOptions[0]}
+                                options={PageSizeOptions}
+                                className={'per-page-selector d-inline-block ml-50 mr-1'}
+                                onChange={handlePageSizeChanged}
+                            />
+                        </Col>
+                        <Col md={6} className={'d-flex align-items-center justify-content-end'}>
+                        </Col>
+                    </Row>
+                </div>
+                <BootstrapTable classes={'table-layout-fixed position-relative b-table'} striped hover columns={columns} bootstrap4
+                                data={list||[]} keyField={'id'}/>
             </Card>
         </>
     );
