ca813cdfa9bd607abe7895c8875f4720c6258ef6
18612365082 <bojinhong@126.com>
2022-11-25 00:32:48 +0800

    框架初始化设置

--- adminui/app/pages/system/departs/DepartTreeList.tsx
+++ adminui/app/pages/system/departs/DepartTreeList.tsx
@@ -1,11 +1,13 @@
-import {Card} from "react-bootstrap";
+import {Button, Card, Col, Form, FormControl, FormGroup, FormLabel, InputGroup, Row} from "react-bootstrap";
 import {useContext, useEffect, useState} from "react";
 import {useFetcher} from "@remix-run/react";
 import CheckboxTree from 'react-checkbox-tree';
-import {defaultTreeIcons, findTree, showToastSuccess, tree2List} from "~/utils/utils";
+import {defaultTreeIcons, findTree, PageSizeOptions, showToastSuccess, tree2List} from "~/utils/utils";
 import themeConfig from "../../../../themeConfig";
 //@ts-ignore
 import _ from 'lodash';
+import ReactSelectThemed from "~/components/react-select-themed/ReactSelectThemed";
+import {Plus, Search, Trash2} from "react-feather";
 
 
 const translateTreeToNode = (treeNode: any) => {
@@ -32,11 +34,29 @@ const DepartTreeList = (props: any) => {
     }, []);
 
 
+    const handleOnAdd = () => {
+
+    }
+    const handleOnAddChild = () => {
+
+    }
+
     return (
         <Card>
             <Card.Header>
                 <Card.Title>所有部门</Card.Title>
             </Card.Header>
+            <div className={'m-2'}>
+                <Row>
+                    <Col md={6} className={'d-flex align-items-center justify-content-start mb-1 mb-md-0'}>
+                        <Button className={'mr-1'} onClick={handleOnAdd}><Plus size={14} style={{marginRight: 5}} />新增</Button>
+                        <Button onClick={handleOnAddChild}><Plus size={14} style={{marginRight: 5}} />添加下级</Button>
+                    </Col>
+                    <Col md={6} className={'d-flex align-items-center justify-content-end'}>
+                        <Button variant={'danger'} disabled={checked.length == 0} type={'button'}><Trash2 size={14} style={{marginRight: 5}} /> 删除</Button>
+                    </Col>
+                </Row>
+            </div>
             <Card.Body>
                 <CheckboxTree
                     nodes={treeData}
