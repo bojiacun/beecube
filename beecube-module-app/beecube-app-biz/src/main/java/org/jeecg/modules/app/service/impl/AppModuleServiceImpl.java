24b8bedca94f497ff1073e6540525eec76643457
18612365082 <bojinhong@126.com>
2022-12-05 10:58:47 +0800

    完善框架

--- beecube-module-app/beecube-app-biz/src/main/java/org/jeecg/modules/app/service/impl/AppModuleServiceImpl.java
+++ beecube-module-app/beecube-app-biz/src/main/java/org/jeecg/modules/app/service/impl/AppModuleServiceImpl.java
@@ -2,6 +2,8 @@ package org.jeecg.modules.app.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.apache.commons.lang3.StringUtils;
+import org.jeecg.common.exception.JeecgBootException;
 import org.jeecg.modules.app.entity.AppModule;
 import org.jeecg.modules.app.mapper.AppModuleMapper;
 import org.jeecg.modules.app.service.IAppModuleService;
@@ -24,5 +26,15 @@ public class AppModuleServiceImpl extends ServiceImpl<AppModuleMapper, AppModule
     @Override
     public AppModule queryByIdentify(String identify) {
         return this.getOne(new LambdaQueryWrapper<AppModule>().eq(AppModule::getIdentify, identify));
+    }
+
+
+    @Override
+    public boolean save(AppModule entity) {
+        //查询identify是否存在，有了则报错
+        if(StringUtils.isEmpty(entity.getId()) && queryByIdentify(entity.getIdentify()) != null) {
+            throw new JeecgBootException("已存在相同标识的模块:"+entity.getIdentify());
+        }
+        return super.save(entity);
     }
 }
