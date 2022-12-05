652687fb5f421b0a67632a923dc6bf77a6648331
18612365082 <bojinhong@126.com>
2022-12-05 10:55:01 +0800

    完善框架

--- beecube-module-app/beecube-app-biz/src/main/java/org/jeecg/modules/app/service/impl/AppModuleServiceImpl.java
+++ beecube-module-app/beecube-app-biz/src/main/java/org/jeecg/modules/app/service/impl/AppModuleServiceImpl.java
@@ -1,11 +1,15 @@
 package org.jeecg.modules.app.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.jeecg.modules.app.entity.AppModule;
 import org.jeecg.modules.app.mapper.AppModuleMapper;
 import org.jeecg.modules.app.service.IAppModuleService;
 import org.springframework.stereotype.Service;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+import javax.annotation.Resource;
 
 /**
  * @Description: 应用模块
@@ -16,8 +20,9 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 @Service
 public class AppModuleServiceImpl extends ServiceImpl<AppModuleMapper, AppModule> implements IAppModuleService {
 
+
     @Override
     public AppModule queryByIdentify(String identify) {
-        return null;
+        return this.getOne(new LambdaQueryWrapper<AppModule>().eq(AppModule::getIdentify, identify));
     }
 }
