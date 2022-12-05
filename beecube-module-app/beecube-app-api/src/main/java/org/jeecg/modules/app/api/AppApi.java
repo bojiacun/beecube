2449be9fd3cdce3667c1ef7744eb5fe1af3b97ae
18612365082 <bojinhong@126.com>
2022-12-05 09:55:02 +0800

    完善框架

--- beecube-module-app/beecube-app-api/src/main/java/org/jeecg/modules/app/api/AppApi.java
+++ beecube-module-app/beecube-app-api/src/main/java/org/jeecg/modules/app/api/AppApi.java
@@ -1,7 +1,11 @@
 package org.jeecg.modules.app.api;
 
+import org.jeecg.modules.app.entity.AppModule;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(value = "beecube-app")
 public interface AppApi {
+    @PostMapping("/app/modules/register")
+    void registerModule(AppModule module);
 }
