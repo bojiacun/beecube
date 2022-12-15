ce9f43b5900293ef3d855a4e231e106846df113e
18612365082 <bojinhong@126.com>
2022-12-15 23:26:15 +0800

    完善框架

--- beecube-module-app/beecube-app-api/src/main/java/cn/winkt/modules/app/api/AppApi.java
+++ beecube-module-app/beecube-app-api/src/main/java/cn/winkt/modules/app/api/AppApi.java
@@ -1,5 +1,6 @@
 package cn.winkt.modules.app.api;
 
+import cn.winkt.modules.app.constant.AppModuleConstants;
 import org.jeecg.common.api.vo.Result;
 import cn.winkt.modules.app.entity.AppModule;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "beecube-app")
+@FeignClient(value = AppModuleConstants.SERVICE_APP)
 public interface AppApi {
     @PostMapping("/app/modules/register")
     Result<?> registerModule(@RequestBody AppModule module);
---
+++ beecube-module-app/beecube-app-api/src/main/java/cn/winkt/modules/app/constant/AppModuleConstants.java
@@ -0,0 +1,6 @@
+package cn.winkt.modules.app.constant;
+
+public interface AppModuleConstants {
+
+    String SERVICE_APP = "beecube-app";
+}
