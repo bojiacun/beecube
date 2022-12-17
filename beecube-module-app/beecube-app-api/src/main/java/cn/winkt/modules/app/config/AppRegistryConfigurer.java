a073540f0896e8336fb9581c73e83e1b7a7caeb4
18612365082 <bojinhong@126.com>
2022-12-17 15:06:49 +0800

    完善框架

---
+++ beecube-module-app/beecube-app-api/src/main/java/cn/winkt/modules/app/config/AppRegistryConfigurer.java
@@ -0,0 +1,68 @@
+package cn.winkt.modules.app.config;
+
+import cn.winkt.modules.app.api.AppApi;
+import cn.winkt.modules.app.vo.AppModule;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
+import org.jeecg.common.api.vo.Result;
+import org.jeecg.common.config.mqtoken.UserTokenContext;
+import org.jeecg.common.constant.CommonConstant;
+import org.jeecg.common.system.util.JwtUtil;
+import org.jeecg.common.util.RedisUtil;
+import org.jeecg.common.util.SpringContextUtils;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Slf4j
+public abstract class AppRegistryConfigurer implements ApplicationRunner {
+    @Resource
+    protected AppApi appApi;
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        UserTokenContext.setToken(getTemporaryToken());
+        String identity = getModuleName();
+        if(!appApi.moduleIsRegistered(identity)) {
+            AppModule appModule = buildModule();
+            Result<?> result = appApi.registerModule(appModule);
+            if(!result.isSuccess()) {
+                log.error("安装模块 {} 出错: {}", identity, result.getMessage());
+            }
+            else {
+                log.info("安装模块 {} 成功", identity);
+            }
+        }
+        else {
+            //升级操作
+        }
+        UserTokenContext.remove();
+    }
+
+
+    /**
+     * 获取临时令牌
+     *
+     * 模拟登陆接口，获取模拟 Token
+     * @return
+     */
+    private static String getTemporaryToken() {
+        RedisUtil redisUtil = SpringContextUtils.getBean(RedisUtil.class);
+        //模拟登录生成临时Token
+        //参数说明：第一个参数是用户名、第二个参数是密码的加密串
+        String token = JwtUtil.sign("admin","cb362cfeefbf3d8d");
+        // 设置Token缓存有效时间为 5 分钟
+        redisUtil.set(CommonConstant.PREFIX_USER_TOKEN + token, token);
+        redisUtil.expire(CommonConstant.PREFIX_USER_TOKEN + token, 5 * 60 * 1000);
+        return token;
+    }
+
+    protected abstract String getModuleName();
+    protected abstract AppModule buildModule() throws IOException;
+}
--- beecube-module-paimai/beecube-module-paimai-biz/src/main/java/cn/winkt/modules/paimai/config/AutoRegisterModule.java
+++ beecube-module-paimai/beecube-module-paimai-biz/src/main/java/cn/winkt/modules/paimai/config/AutoRegisterModule.java
@@ -1,5 +1,6 @@
 package cn.winkt.modules.paimai.config;
 
+import cn.winkt.modules.app.config.AppRegistryConfigurer;
 import cn.winkt.modules.app.vo.AppModule;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
@@ -17,73 +18,44 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 import java.io.InputStream;
 
 @Component
 @Slf4j
-public class AutoRegisterModule implements ApplicationRunner {
+public class AutoRegisterModule extends AppRegistryConfigurer {
 
-    public static final String MODULE_IDENTITY = "paimai";
 
-    @Resource
-    private AppApi appApi;
-
-    //自动往app注册模块
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-        UserTokenContext.setToken(getTemporaryToken());
-        if(!appApi.moduleIsRegistered(MODULE_IDENTITY)) {
-            AppModule appModule = new AppModule();
-            appModule.setIdentify(MODULE_IDENTITY);
-            appModule.setName("汇智拍卖");
-            ClassPathResource logoResource = new ClassPathResource("static/logo.jpeg");
-            ClassPathResource manifestResouce = new ClassPathResource("static/manifest.json");
-            InputStream imageStream = logoResource.getInputStream();
-            byte[] buffer = new byte[(int)logoResource.getFile().length()];
-            IOUtils.readFully(imageStream, buffer);
-            imageStream.close();
-            byte[] manifestBuffer = new byte[(int)manifestResouce.getFile().length()];
-            IOUtils.readFully(manifestResouce.getInputStream(), manifestBuffer);
-            manifestResouce.getInputStream().close();
-
-
-            appModule.setLogo("data:image/jpeg;base64,"+new String(Base64.encodeBase64(buffer)));
-            appModule.setAuthor("Mr Bo");
-            appModule.setStatus(0);
-            appModule.setSupportWechat(1);
-            appModule.setSupportH5(1);
-            appModule.setSupportDouyin(0);
-            appModule.setVersion("1.0.0");
-            appModule.setNewVersion("1.0.0");
-            appModule.setManifest(new String(manifestBuffer));
-            Result<?> result = appApi.registerModule(appModule);
-            if(!result.isSuccess()) {
-                log.error("安装模块 {} 出错: {}", MODULE_IDENTITY, result.getMessage());
-            }
-            else {
-                log.info("安装模块 {} 成功", MODULE_IDENTITY);
-            }
-        }
-        else {
-            //升级操作
-
-        }
-        UserTokenContext.remove();
+    protected String getModuleName() {
+        return "paimai";
     }
-    /**
-     * 获取临时令牌
-     *
-     * 模拟登陆接口，获取模拟 Token
-     * @return
-     */
-    private static String getTemporaryToken() {
-        RedisUtil redisUtil = SpringContextUtils.getBean(RedisUtil.class);
-        //模拟登录生成临时Token
-        //参数说明：第一个参数是用户名、第二个参数是密码的加密串
-        String token = JwtUtil.sign("admin","cb362cfeefbf3d8d");
-        // 设置Token缓存有效时间为 5 分钟
-        redisUtil.set(CommonConstant.PREFIX_USER_TOKEN + token, token);
-        redisUtil.expire(CommonConstant.PREFIX_USER_TOKEN + token, 5 * 60 * 1000);
-        return token;
+
+    @Override
+    protected AppModule buildModule() throws IOException {
+        AppModule appModule = new AppModule();
+        appModule.setIdentify(getModuleName());
+        appModule.setName("汇智拍卖");
+        ClassPathResource logoResource = new ClassPathResource("static/logo.jpeg");
+        ClassPathResource manifestResouce = new ClassPathResource("static/manifest.json");
+        InputStream imageStream = logoResource.getInputStream();
+        byte[] buffer = new byte[(int) logoResource.getFile().length()];
+        IOUtils.readFully(imageStream, buffer);
+        imageStream.close();
+        byte[] manifestBuffer = new byte[(int) manifestResouce.getFile().length()];
+        IOUtils.readFully(manifestResouce.getInputStream(), manifestBuffer);
+        manifestResouce.getInputStream().close();
+
+
+        appModule.setLogo("data:image/jpeg;base64," + new String(Base64.encodeBase64(buffer)));
+        appModule.setAuthor("Mr Bo");
+        appModule.setStatus(0);
+        appModule.setSupportWechat(1);
+        appModule.setSupportH5(1);
+        appModule.setSupportDouyin(0);
+        appModule.setVersion("1.0.0");
+        appModule.setNewVersion("1.0.0");
+        appModule.setManifest(new String(manifestBuffer));
+        return appModule;
     }
 }
