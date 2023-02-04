0e7856f35b9a45f9010f610b933f200a5f6b1279
18612365082 <bojinhong@126.com>
2023-02-04 15:02:44 +0800

    完善框架

--- beecube-module-app/beecube-app-api/src/main/java/cn/winkt/modules/app/config/AppRealmConfiguration.java
+++ beecube-module-app/beecube-app-api/src/main/java/cn/winkt/modules/app/config/AppRealmConfiguration.java
@@ -1,8 +1,10 @@
 package cn.winkt.modules.app.config;
 
+import org.apache.shiro.realm.Realm;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.jeecg.common.constant.CommonConstant;
+import org.jeecg.config.shiro.ShiroConfigurer;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -10,27 +12,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 import javax.annotation.Resource;
+import javax.servlet.Filter;
+import java.util.*;
 
 @Configuration
-public class AppRealmConfiguration implements ApplicationRunner {
-
-    @Resource
-    private DefaultWebSecurityManager defaultWebSecurityManager;
-
-    @Resource
-    ShiroFilterFactoryBean shiroFilterFactoryBean;
+public class AppRealmConfiguration implements ShiroConfigurer {
 
     @Resource
     Environment env;
 
-    @Bean
-    public void extendShiroConfig() {
-        Object cloudServer = env.getProperty(CommonConstant.CLOUD_SERVER_KEY);
-        shiroFilterFactoryBean.getFilters().put("wechat", new WechatJwtFilter(cloudServer == null));
-        defaultWebSecurityManager.getRealms().add(new AppRealm());
+
+
+    @Override
+    public Collection<Realm> extendRealms() {
+        return Collections.singletonList(new AppRealm());
     }
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public Map<String, Filter> extendFilters() {
+        Map<String, Filter> map = new HashMap<>();
+        Object cloudServer = env.getProperty(CommonConstant.CLOUD_SERVER_KEY);
+        map.put("wechat", new WechatJwtFilter(cloudServer == null));
+        return map;
     }
 }
