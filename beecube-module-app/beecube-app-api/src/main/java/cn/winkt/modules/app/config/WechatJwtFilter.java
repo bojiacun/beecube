e9df8fbbe9c48ab8c4702ab5bf4f73b5cf6e922d
18612365082 <bojinhong@126.com>
2023-02-04 16:19:33 +0800

    完善框架

--- beecube-module-app/beecube-app-api/src/main/java/cn/winkt/modules/app/config/WechatJwtFilter.java
+++ beecube-module-app/beecube-app-api/src/main/java/cn/winkt/modules/app/config/WechatJwtFilter.java
@@ -1,27 +1,36 @@
 package cn.winkt.modules.app.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.apache.shiro.web.filter.AccessControlFilter;
+import org.jeecg.common.config.TenantContext;
 import org.jeecg.common.constant.CommonConstant;
 import org.jeecg.common.system.util.JwtUtil;
 import org.jeecg.common.util.oConvertUtils;
+import org.jeecg.config.AppContext;
 import org.jeecg.config.shiro.JwtToken;
 import org.jeecg.config.shiro.LoginType;
 import org.jeecg.config.shiro.filters.JwtFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
-public class WechatJwtFilter extends JwtFilter {
+public class WechatJwtFilter extends AccessControlFilter {
 
     public WechatJwtFilter() {
+        this(true);
     }
-
     public WechatJwtFilter(boolean allowOrigin) {
-        super(allowOrigin);
+        this.allowOrigin = allowOrigin;
     }
 
+    private boolean allowOrigin = true;
     @Override
     protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
         try {
@@ -34,6 +43,10 @@ public class WechatJwtFilter extends JwtFilter {
     }
 
     @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+        return false;
+    }
+
     protected boolean executeLogin(ServletRequest request, ServletResponse response) throws Exception {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         String token = httpServletRequest.getHeader(CommonConstant.X_ACCESS_TOKEN);
@@ -48,5 +61,38 @@ public class WechatJwtFilter extends JwtFilter {
         getSubject(request, response).login(jwtToken);
         // 如果没有抛出异常则代表登入成功，返回true
         return true;
+    }
+
+    @Override
+    protected boolean preHandle(ServletRequest request, ServletResponse response) throws Exception {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        if(allowOrigin){
+            httpServletResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, httpServletRequest.getHeader(HttpHeaders.ORIGIN));
+            // 允许客户端请求方法
+            httpServletResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET,POST,OPTIONS,PUT,DELETE");
+            // 允许客户端提交的Header
+            String requestHeaders = httpServletRequest.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS);
+            if (StringUtils.isNotEmpty(requestHeaders)) {
+                httpServletResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, requestHeaders);
+            }
+            // 允许客户端携带凭证信息(是否允许发送Cookie)
+            httpServletResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+        }
+        // 跨域时会首先发送一个option请求，这里我们给option请求直接返回正常状态
+        if (RequestMethod.OPTIONS.name().equalsIgnoreCase(httpServletRequest.getMethod())) {
+            httpServletResponse.setStatus(HttpStatus.OK.value());
+            return false;
+        }
+        //update-begin-author:taoyan date:20200708 for:多租户用到
+        String tenantId = httpServletRequest.getHeader(CommonConstant.TENANT_ID);
+        TenantContext.setTenant(tenantId);
+        //update-end-author:taoyan date:20200708 for:多租户用到
+
+        //设置APP
+        String appId = httpServletRequest.getHeader(CommonConstant.X_APP_ID);
+        AppContext.setApp(appId);
+
+        return super.preHandle(request, response);
     }
 }
