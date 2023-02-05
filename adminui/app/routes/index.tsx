667d069d2183a4c86d578cc2bb0222aaea40fab1
18612365082 <bojinhong@126.com>
2023-02-05 20:39:27 +0800

    完善框架

--- adminui/app/routes/index.tsx
+++ adminui/app/routes/index.tsx
@@ -9,7 +9,6 @@ export const loader: LoaderFunction = async ({request}) => {
     const perms = userInfo!.perms;
     //从用户的可用菜单中取出第一个菜单并重定向到该菜单地址
     const url = perms == null ? '/': perms[1].route;
-    console.log(perms);
     return redirect(url!);
 }
 
