8c0d177c32a651e3c46fdf65cd5690ad5167e615
18612365082 <bojinhong@126.com>
2023-02-05 17:07:59 +0800

    完善框架

--- jeecg-boot-base-core/src/main/java/org/jeecg/config/RestTemplateConfig.java
+++ jeecg-boot-base-core/src/main/java/org/jeecg/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package org.jeecg.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
         return new RestTemplate(factory);
     }
