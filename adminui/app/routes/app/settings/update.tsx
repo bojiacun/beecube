cb237de861d61fb1040d990f1905be54731da9bb
18612365082 <bojinhong@126.com>
2023-02-07 22:42:03 +0800

    完善框架

--- adminui/app/routes/app/settings/update.tsx
+++ adminui/app/routes/app/settings/update.tsx
@@ -6,11 +6,7 @@ import {formData2Json} from "~/utils/utils";
 export const action: ActionFunction = async ({request}) => {
     await requireAuthenticated(request);
     const url = new URL(request.url);
-    const groupKey = url.searchParams.get("group");
-    if(groupKey == null) throw new Response("参数错误", {status: 500});
     const formData = await request.formData();
     const data = formData2Json(formData);
-    let postData:any = {};
-    postData[groupKey] = JSON.parse(data);
-    return await requestWithToken(request)(API_APP_SETTINGS_UPDATE, postFormInit(JSON.stringify(postData)))
+    return await requestWithToken(request)(API_APP_SETTINGS_UPDATE+'?'+url.searchParams.toString(), postFormInit(data));
 }
