576e5eee5d38978406653dce4305a24ff08368dc
18612365082 <bojinhong@126.com>
2023-02-10 17:00:29 +0800

    完善框架

--- adminui/app/pages/paimai/GoodsEditor.tsx
+++ adminui/app/pages/paimai/GoodsEditor.tsx
@@ -57,10 +57,9 @@ const GoodsEditor = (props: any) => {
     //拍品分类
     useEffect(() => {
         if (goodsClassFetcher.type === 'done' && goodsClassFetcher.data) {
-            setGoodsClassOptions(goodsClassFetcher.data.map((item:any)=>({label: item.name, value: item.id})));
+            setGoodsClassOptions(goodsClassFetcher.data.map((item: any) => ({label: item.name, value: item.id})));
         }
     }, [goodsClassFetcher.state]);
-
 
 
     useEffect(() => {
@@ -88,74 +87,70 @@ const GoodsEditor = (props: any) => {
                 <Modal.Header closeButton>
                     <Modal.Title id={'edit-user-model'}>{model?.id ? '编辑' : '新建'}拍品</Modal.Title>
                 </Modal.Header>
-                {model &&
-                    <Formik innerRef={formikRef} initialValues={{status: 0,type: 1, endTime: '', images: '', ...model}} validationSchema={GoodsSchema} onSubmit={handleOnSubmit}>
-                        {(formik)=>{
-                            return (
-                                <Form method={'post'}>
-                                    <Modal.Body style={{maxHeight: 'calc(100vh - 200px)', overflowY: 'auto'}}>
-                                        <BootstrapSelect name={'class_id'} label={'分类'} options={goodsClassOptions} />
-                                        <BootstrapInput label={'标题'} name={'title'}/>
-                                        <BootstrapInput label={'副标题'} name={'subTitle'}/>
-                                        <FormGroup>
-                                            <FormLabel htmlFor={'images'}>拍品图片</FormLabel>
-                                            <FileBrowserInput name={'images'} type={1} multi={true} />
-                                        </FormGroup>
+                <Formik innerRef={formikRef} initialValues={{status: 0, type: 1, endTime: '', images: '', ...model}} validationSchema={GoodsSchema}
+                        onSubmit={handleOnSubmit}>
+                    <Form method={'post'}>
+                        <Modal.Body style={{maxHeight: 'calc(100vh - 200px)', overflowY: 'auto'}}>
+                            <BootstrapSelect name={'class_id'} label={'分类'} options={goodsClassOptions}/>
+                            <BootstrapInput label={'标题'} name={'title'}/>
+                            <BootstrapInput label={'副标题'} name={'subTitle'}/>
+                            <FormGroup>
+                                <FormLabel htmlFor={'images'}>拍品图片</FormLabel>
+                                <FileBrowserInput name={'images'} type={1} multi={true}/>
+                            </FormGroup>
 
-                                        <BootstrapDateTime label={'结束时间'} name={'endTime'} showTime={true} />
-                                        <BootstrapInput label={'起拍价'} name={'startPrice'} />
-                                        <BootstrapInput label={'保证金'} name={'deposit'} placeholder={'保证金（元）'} />
-                                        <BootstrapInput label={'延时周期'} name={'delayTime'} placeholder={'延时周期（分钟）'} />
-                                        <BootstrapRadioGroup  options={[{label: '普通拍品', value: '1'},{label: '一口价', value: '2'}]} name={'type'} label={'拍品类型'} />
+                            <BootstrapDateTime label={'结束时间'} name={'endTime'} showTime={true}/>
+                            <BootstrapInput label={'起拍价'} name={'startPrice'}/>
+                            <BootstrapInput label={'保证金'} name={'deposit'} placeholder={'保证金（元）'}/>
+                            <BootstrapInput label={'延时周期'} name={'delayTime'} placeholder={'延时周期（分钟）'}/>
+                            <BootstrapRadioGroup options={[{label: '普通拍品', value: '1'}, {label: '一口价', value: '2'}]} name={'type'}
+                                                 label={'拍品类型'}/>
 
-                                        <FormGroup>
-                                            <FormLabel htmlFor={'post'}>加价配置</FormLabel>
-                                            <Row>
-                                                <Col sm={10}>
-                                                </Col>
-                                                <Col sm={2}>
-                                                    <Button onClick={() => setUprangConfigerShow(true)}>点击配置</Button>
-                                                </Col>
-                                            </Row>
-                                        </FormGroup>
-                                        <FormGroup>
-                                            <FormLabel htmlFor={'desc_flow'}>拍品流程</FormLabel>
-                                            <TinymceEditor name={'desc_flow'} />
-                                        </FormGroup>
-                                        <FormGroup>
-                                            <FormLabel htmlFor={'desc_delivery'}>物流运输</FormLabel>
-                                            <TinymceEditor name={'desc_delivery'} />
-                                        </FormGroup>
-                                        <FormGroup>
-                                            <FormLabel htmlFor={'desc_notice'}>注意事项</FormLabel>
-                                            <TinymceEditor name={'desc_notice'} />
-                                        </FormGroup>
-                                        <FormGroup>
-                                            <FormLabel htmlFor={'desc_read'}>拍卖须知</FormLabel>
-                                            <TinymceEditor name={'desc_read'} />
-                                        </FormGroup>
-                                        <FormGroup>
-                                            <FormLabel htmlFor={'desc_deposit'}>保证金说明</FormLabel>
-                                            <TinymceEditor name={'desc_deposit'} />
-                                        </FormGroup>
-                                        <BootstrapRadioGroup  options={[{label: '下架', value: '0'},{label: '上架', value: '1'}]} name={'status'} label={'状态'} />
-                                    </Modal.Body>
-                                    <Modal.Footer>
-                                        <Button
-                                            variant={'primary'}
-                                            disabled={formik.isSubmitting}
-                                            type={'submit'}
-                                        >
-                                            保存
-                                        </Button>
-                                    </Modal.Footer>
-                                </Form>
-                            );
-                        }}
-                    </Formik>
-                }
+                            <FormGroup>
+                                <FormLabel htmlFor={'post'}>加价配置</FormLabel>
+                                <Row>
+                                    <Col sm={10}>
+                                    </Col>
+                                    <Col sm={2}>
+                                        <Button onClick={() => setUprangConfigerShow(true)}>点击配置</Button>
+                                    </Col>
+                                </Row>
+                            </FormGroup>
+                            <FormGroup>
+                                <FormLabel htmlFor={'desc_flow'}>拍品流程</FormLabel>
+                                <TinymceEditor name={'desc_flow'}/>
+                            </FormGroup>
+                            <FormGroup>
+                                <FormLabel htmlFor={'desc_delivery'}>物流运输</FormLabel>
+                                <TinymceEditor name={'desc_delivery'}/>
+                            </FormGroup>
+                            <FormGroup>
+                                <FormLabel htmlFor={'desc_notice'}>注意事项</FormLabel>
+                                <TinymceEditor name={'desc_notice'}/>
+                            </FormGroup>
+                            <FormGroup>
+                                <FormLabel htmlFor={'desc_read'}>拍卖须知</FormLabel>
+                                <TinymceEditor name={'desc_read'}/>
+                            </FormGroup>
+                            <FormGroup>
+                                <FormLabel htmlFor={'desc_deposit'}>保证金说明</FormLabel>
+                                <TinymceEditor name={'desc_deposit'}/>
+                            </FormGroup>
+                            <BootstrapRadioGroup options={[{label: '下架', value: '0'}, {label: '上架', value: '1'}]} name={'status'} label={'状态'}/>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button
+                                variant={'primary'}
+                                disabled={postFetcher.state === 'submitting'}
+                                type={'submit'}
+                            >
+                                保存
+                            </Button>
+                        </Modal.Footer>
+                    </Form>
+                </Formik>
             </Modal>
-            <UprangConfiger show={uprangConfigerShow} onHide={()=>setUprangConfigerShow(false)} />
+            <UprangConfiger show={uprangConfigerShow} onHide={() => setUprangConfigerShow(false)}/>
         </>
     );
 }
