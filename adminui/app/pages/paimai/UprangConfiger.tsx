928764a6082293ec68bcada7213a0fdaf9b6ce27
18612365082 <bojinhong@126.com>
2023-02-10 22:09:34 +0800

    完善框架

--- adminui/app/pages/paimai/UprangConfiger.tsx
+++ adminui/app/pages/paimai/UprangConfiger.tsx
@@ -1,7 +1,9 @@
 import {Button, Modal, Row, Col, FormControl, Alert} from "react-bootstrap";
-import {FC, useState} from "react";
+import {FC, useEffect, useState} from "react";
 import {useFormikContext} from "formik";
 import {PlusCircle} from "react-feather";
+import {useTranslation} from "react-i18next";
+import classNames from "classnames";
 
 export interface UprangeConfigerProps {
     name: string;
@@ -13,15 +15,33 @@ const UprangConfiger: FC<UprangeConfigerProps> = (props) => {
     const [list, setList] = useState<any[]>([]);
     const [show, setShow] = useState<boolean>(false);
     const formik = useFormikContext<any>();
+    const {t} = useTranslation();
+
+    useEffect(()=>{
+        let initValue = formik.values[name];
+        if(initValue) {
+            setList(JSON.parse(initValue));
+        }
+    }, []);
 
     const handleOnAdd = () => {
         list.push({min: 0, max: 0, price: 0});
         setList([...list]);
     }
 
+    const handleOnSubmit = () => {
+        formik.setFieldValue(name, JSON.stringify(list));
+    }
+
+    const hanldeOnItemChange = (e:any, item:any, key:string) => {
+        item[key] = e.target.value;
+        setList([...list]);
+    }
+
     return (
         <>
-            <Button onClick={() => setShow(true)}>{label}</Button>
+            <Button onClick={() => setShow(true)} className={classNames((!!formik.errors[name]) ? 'is-invalid':'')}>{label}</Button>
+            {formik.errors[name]&&<FormControl.Feedback type={'invalid'}>{t(formik.errors[name]!.toString())}</FormControl.Feedback>}
             <Modal
                 show={show}
                 onHide={() => setShow(false)}
@@ -37,12 +57,12 @@ const UprangConfiger: FC<UprangeConfigerProps> = (props) => {
                         <Col md={4}>区间（右）</Col>
                         <Col md={4}>加价</Col>
                     </Row>
-                    {list.map(item=>{
+                    {list.map((item,index)=>{
                         return (
-                            <Row className={'mb-1'}>
-                                <Col md={4}><FormControl value={item.min} /></Col>
-                                <Col md={4}><FormControl value={item.max} /></Col>
-                                <Col md={4}><FormControl value={item.price} /></Col>
+                            <Row className={'mb-1'} key={'range-'+index}>
+                                <Col md={4}><FormControl value={item.min} onChange={e => hanldeOnItemChange(e, item, 'min')} /></Col>
+                                <Col md={4}><FormControl value={item.max} onChange={e => hanldeOnItemChange(e, item, 'max')} /></Col>
+                                <Col md={4}><FormControl value={item.price} onChange={e => hanldeOnItemChange(e, item, 'price')} /></Col>
                             </Row>
                         );
                     })}
@@ -51,10 +71,7 @@ const UprangConfiger: FC<UprangeConfigerProps> = (props) => {
                     </Row>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button
-                        variant={'primary'}
-                        type={'submit'}
-                    >
+                    <Button variant={'primary'} onClick={handleOnSubmit}>
                         保存
                     </Button>
                 </Modal.Footer>
