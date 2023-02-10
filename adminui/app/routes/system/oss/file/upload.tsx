f5f31c9a8cb9111fc4e344cfcb66851605f1bf22
18612365082 <bojinhong@126.com>
2023-02-10 08:58:33 +0800

    完善框架

--- adminui/app/routes/system/oss/file/upload.tsx
+++ adminui/app/routes/system/oss/file/upload.tsx
@@ -1,4 +1,4 @@
-import {ActionFunction} from "@remix-run/node";
+import {ActionFunction, json} from "@remix-run/node";
 import {requireAuthenticated} from "~/utils/auth.server";
 import {API_OSS_FILE_UPLOAD, requestWithToken} from "~/utils/request.server";
 
@@ -10,7 +10,13 @@ export const action: ActionFunction = async ({request}) => {
     data.append("type", formData.get("type"));
     //@ts-ignore
     data.append('file', formData.get('file'));
-    return await requestWithToken(request)(
+    let result = await requestWithToken(request)(
         API_OSS_FILE_UPLOAD, {method: 'post', body: data}
     );
+    return json({
+        code: result.code,
+        message: result.message,
+        success: result.success,
+        data: result.result.url
+    });
 }
