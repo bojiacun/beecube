2b39cbf03a72dc233484ba61ffdc2d8750444c55
18612365082 <bojinhong@126.com>
2023-02-13 23:05:55 +0800

    完善框架

--- wxapp/src/app.tsx
+++ wxapp/src/app.tsx
@@ -1,21 +1,30 @@
-import { Component, PropsWithChildren } from 'react'
+import {Component, PropsWithChildren} from 'react'
+import {Provider} from 'react-redux';
 import "windi.css";
 import './app.scss';
 import configStore from "./store";
+
 const QQMapWX = require('./lib/qqmap-wx-jssdk.min');
 const store = configStore();
 
 class App extends Component<PropsWithChildren> {
 
-  componentDidMount () {}
+  componentDidMount() {
+  }
 
-  componentDidShow () {}
+  componentDidShow() {
+  }
 
-  componentDidHide () {}
+  componentDidHide() {
+  }
 
-  render () {
+  render() {
     // this.props.children 是将要会渲染的页面
-    return this.props.children
+    return (
+      <Provider store={store}>
+        {this.props.children}
+      </Provider>
+    )
   }
 }
 
