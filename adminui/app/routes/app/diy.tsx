8ab84a737489e51ca3ea24265b35d9cc4ac11dea
18612365082 <bojinhong@126.com>
2023-02-15 13:40:17 +0800

    完善框架

--- adminui/app/routes/app/diy.tsx
+++ adminui/app/routes/app/diy.tsx
@@ -1,7 +1,6 @@
 import {defaultRouteCatchBoundary, defaultRouteErrorBoundary} from "~/utils/utils";
 import {withPageLoading} from "~/utils/components";
 import diyPageStyleUrl from 'app/styles/diy.css';
-import diyModulePageStyleUrl from 'app/styles/diy.module.css';
 import {LinksFunction} from "@remix-run/node";
 import PageDesigner from "~/components/page-designer";
 
@@ -11,7 +10,6 @@ export const CatchBoundary = defaultRouteCatchBoundary;
 export const links: LinksFunction = () => {
     return [
         {rel: 'stylesheet', href: diyPageStyleUrl},
-        {rel: 'stylesheet', href: diyModulePageStyleUrl},
     ];
 }
 
