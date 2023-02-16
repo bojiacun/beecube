87ea13f3db48e217b641c3cf634a48111999789d
18612365082 <bojinhong@126.com>
2023-02-16 14:41:52 +0800

    完善框架

--- adminui/app/components/page-designer/page.tsx
+++ adminui/app/components/page-designer/page.tsx
@@ -1,40 +1,46 @@
-import { presetColors, withSettingsComponent } from "./component";
-import {Formik} from "formik";
+import {AttributeTabs, withSettingsComponent} from "./component";
+import {Form, Formik} from "formik";
+import BootstrapInput from "~/components/form/BootstrapInput";
+import React from "react";
 
 export const DEFAULT_PAGE_DATA = {
     backgroundColor: '#f5f5f5',
-    paddingLeft: 0,
-    paddingRight: 0,
-    paddingTop: 0,
-    paddingBottom: 0,
+    paddingLeft: '0px',
+    paddingRight: '0px',
+    paddingTop: '0px',
+    paddingBottom: '0px',
 };
 
 const PageSettings = (props: any) => {
-    const { onUpdate, data } = props;
-    let _data = { ...DEFAULT_PAGE_DATA, ...data };
+    const {onUpdate, data} = props;
+    let _data = {...DEFAULT_PAGE_DATA, ...data};
+
+    const handleOnSubmit = (values: any) => {
+        onUpdate({...values});
+    }
+
     return (
-        <Formik key="style" initialValues={{ ..._data }} onSubmit={(changedValues, allValues) => {
-        }}>
-            {/*<Collapse expandIconPosition="right" bordered={false} ghost={true} defaultActiveKey={['页面样式']}>*/}
-            {/*    <Collapse.Panel header="页面样式" key="页面样式">*/}
-            {/*        <ProFormColorPicker label="背景颜色" name="backgroundColor" fieldProps={{presetColors: presetColors}} />*/}
-            {/*        <ProFormGroup>*/}
-            {/*            <ProFormSlider label="左内间距" name="paddingLeft" width="xs"  max={30} />*/}
-            {/*            <ProFormSlider label="右内间距" name="paddingRight" width="xs" max={30} />*/}
-            {/*        </ProFormGroup>*/}
-            {/*        <ProFormGroup>*/}
-            {/*            <ProFormSlider label="上内间距" name="paddingTop" width="xs"  max={100} />*/}
-            {/*            <ProFormSlider label="下内间距" name="paddingBottom" width="xs" max={100} />*/}
-            {/*        </ProFormGroup>*/}
-            {/*        <ProFormItem*/}
-            {/*            name="backgroundImageUrl"*/}
-            {/*            label="背景图"*/}
-            {/*        >*/}
-            {/*            <FileBrowserInput type={FILE_TYPE_IMAGE} imagePreview={true} />*/}
-            {/*        </ProFormItem>*/}
-            {/*    </Collapse.Panel>*/}
-            {/*</Collapse>*/}
-        </Formik>
+        <AttributeTabs tabs={['页面设置']}>
+            <div style={{padding: 15}}>
+                <Formik initialValues={{..._data}} onSubmit={handleOnSubmit}>
+                    {
+                        (formik) => {
+                            return (
+                                <Form method={'post'} onChange={(e) => formik.submitForm()}>
+                                    <BootstrapInput label={'页面背景颜色'} name={'backgroundColor'}/>
+                                    <BootstrapInput label={'左边距'} name={'paddingLeft'}/>
+                                    <BootstrapInput label={'上边距'} name={'paddingTop'}/>
+                                    <BootstrapInput label={'右边距'} name={'paddingRight'}/>
+                                    <BootstrapInput label={'下边距'} name={'paddingBottom'}/>
+                                </Form>
+                            );
+                        }
+                    }
+
+                </Formik>
+            </div>
+            <div></div>
+        </AttributeTabs>
     );
 }
 
