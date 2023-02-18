6c3012ae0913f1baff124a8954cc448992056e11
18612365082 <bojinhong@126.com>
2023-02-18 21:09:03 +0800

    完善框架

--- adminui/app/layouts/layout-vertical/components/vertical-nav-menu/VerticalNavMenu.tsx
+++ adminui/app/layouts/layout-vertical/components/vertical-nav-menu/VerticalNavMenu.tsx
@@ -34,7 +34,7 @@ const VerticalNavMenu = (props:any) => {
         wheelPropagation: false,
     }
     const app = rootLoaderData?.app ? JSON.parse(rootLoaderData?.app) : null;
-
+    console.log('root loader data is',rootLoaderData);
     return (
         <div className={classNames('main-menu menu-fixed menu-accordion menu-shadow',
             !isVerticalMenuCollapsed || (isVerticalMenuCollapsed) ? 'expanded':'', skin === 'dark' ? 'menu-dark':'menu-light')}
