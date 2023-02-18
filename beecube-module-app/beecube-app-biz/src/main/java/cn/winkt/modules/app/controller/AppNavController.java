a341b167298edc04242be119ef7a2b2c50901177
18612365082 <bojinhong@126.com>
2023-02-18 23:10:20 +0800

    完善框架

--- beecube-module-app/beecube-app-biz/src/main/java/cn/winkt/modules/app/controller/AppNavController.java
+++ beecube-module-app/beecube-app-biz/src/main/java/cn/winkt/modules/app/controller/AppNavController.java
@@ -9,6 +9,7 @@ import java.net.URLDecoder;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.jeecg.common.api.vo.Result;
+import org.jeecg.common.aspect.annotation.AutoDict;
 import org.jeecg.common.system.query.QueryGenerator;
 import org.jeecg.common.aspect.annotation.AutoLog;
 import org.jeecg.common.util.oConvertUtils;
@@ -61,6 +62,7 @@ public class AppNavController extends JeecgController<AppNav, IAppNavService> {
 	@AutoLog(value = "应用导航表-分页列表查询")
 	@ApiOperation(value="应用导航表-分页列表查询", notes="应用导航表-分页列表查询")
 	@GetMapping(value = "/list")
+	@AutoDict
 	public Result<?> queryPageList(AppNav appNav,
 								   @RequestParam(name="pageNo", defaultValue="1") Integer pageNo,
 								   @RequestParam(name="pageSize", defaultValue="10") Integer pageSize,
