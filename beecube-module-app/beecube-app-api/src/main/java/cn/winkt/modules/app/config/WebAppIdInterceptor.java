bb3f0823092ba3e1d799abcde3ae2677c00ce7f6
18612365082 <bojinhong@126.com>
2023-02-19 23:18:41 +0800

    完善框架

--- beecube-module-app/beecube-app-api/src/main/java/cn/winkt/modules/app/config/WebAppIdInterceptor.java
+++ beecube-module-app/beecube-app-api/src/main/java/cn/winkt/modules/app/config/WebAppIdInterceptor.java
@@ -1,5 +1,6 @@
 package cn.winkt.modules.app.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.jeecg.common.config.TenantContext;
 import org.jeecg.common.constant.CommonConstant;
 import org.jeecg.config.AppContext;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 public class WebAppIdInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) throws Exception {
@@ -19,6 +21,7 @@ public class WebAppIdInterceptor implements HandlerInterceptor {
         String appId = request.getHeader(CommonConstant.X_APP_ID);
         AppContext.setApp(appId);
 
+        log.info("设置Header中的AppId和TenantId {} {}", appId, tenantId);
         return true;
     }
 }
