69398e5e0ae74556fdb60fb924fa41988f10a75c
18612365082 <bojinhong@126.com>
2023-02-24 17:53:10 +0800

    完善框架

--- wxapp/src/pages/my/index.tsx
+++ wxapp/src/pages/my/index.tsx
@@ -69,8 +69,8 @@ export default class Index extends Component<PropsWithChildren<any>> {
               <View>足迹</View>
             </View>
           </View>
-          <View className={'rounded-md bg-white mt-2 text-blue-600 py-2.5'}>
-            <View className={'grid grid-cols-4 gap-1 text-center'}>
+          <View className={'rounded-md bg-white mt-2 text-blue-600 py-4'}>
+            <View className={'grid grid-cols-5 gap-1 text-center'}>
               <View>
                 <View className={'iconfont icon-daizhifudingdan'} style={{fontSize: 24}} />
                 <View className={'text-black'}>待结算</View>
@@ -87,24 +87,28 @@ export default class Index extends Component<PropsWithChildren<any>> {
                 <View className={'iconfont icon-yiwancheng'} style={{fontSize: 24}} />
                 <View className={'text-black'}>已完成</View>
               </View>
+              <View>
+                <View className={'iconfont icon-quanbu'} style={{fontSize: 24}} />
+                <View className={'text-black'}>全部</View>
+              </View>
             </View>
           </View>
-          <View className={'mt-2 p-2'}>
-            <View className={'flex items-center justify-between'}>
-              <View className={'flex items-center space-x-2'}>
-                <View className={'iconfont icon-qianbao'} style={{fontSize: 24}} />
-                <View>我的余额</View>
+
+        </View>
+        <View className={'mt-4 p-2 bg-white'}>
+          <View className={'flex items-center justify-between'}>
+            <View className={'flex items-center space-x-2'}>
+              <View className={'iconfont icon-qianbao'} style={{fontSize: 24}} />
+              <View>我的余额</View>
+            </View>
+            <View className={'flex items-center space-x-2'}>
+              <View>
+                RMB 0
               </View>
-              <View className={'flex items-center space-x-2'}>
-                <View>
-                  RMB 0
-                </View>
-                <View className={'iconfont icon-youjiantou_huaban'} />
-              </View>
+              <View className={'iconfont icon-youjiantou_huaban'} />
             </View>
           </View>
         </View>
-
       </PageLayout>
     )
   }
