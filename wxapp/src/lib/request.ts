0e63d686f0ad365e7360a9853a8ba67d2b134139
18612365082 <bojinhong@126.com>
2023-02-25 08:33:34 +0800

    完善框架

--- wxapp/src/lib/request.ts
+++ wxapp/src/lib/request.ts
@@ -1,28 +1,32 @@
 // api.js
 import axios from "taro-axios";
 import Taro from "@tarojs/taro";
+import utils from "./utils";
 
 const siteinfo = require('../siteinfo');
 const API_URL = siteinfo.siteroot;
 
 const instance = axios.create({
-  baseURL: API_URL,
-  timeout: 10000,
-  headers: {'X-App-Id': siteinfo.appId},
+    baseURL: API_URL,
+    timeout: 10000,
+    headers: {'X-App-Id': siteinfo.appId},
 });
 
-const hanldeOnResponseError = (error:any) => {
-  if(!error.status) {
-    return Taro.showToast({icon: 'none', title: '网络请求发生错误!', duration: 1500}).then();
-  }
+const handleOnResponseError = (error: any) => {
+    if (!error.status) {
+        return Taro.showToast({icon: 'none', title: '网络请求发生错误!', duration: 1500}).then();
+    }
+    else {
+    }
+    utils.hideLoading();
 }
 
-instance.interceptors.response.use((response)=>{
-  return response;
-}, (error)=>{
-  // console.log(error);
-  hanldeOnResponseError(error);
-  return Promise.reject(error);
+instance.interceptors.response.use((response) => {
+    utils.hideLoading();
+    return response;
+}, (error) => {
+    handleOnResponseError(error);
+    return Promise.reject(error);
 });
 
 export default instance;
--- wxapp/src/lib/utils.tsx
+++ wxapp/src/lib/utils.tsx
@@ -1,95 +1,101 @@
 import Taro from "@tarojs/taro";
 
 const indexOf = (arr, value) => {
-  let exists = false;
-  arr.forEach(v => {
-    if (value.match(v)) {
-      exists = true;
-    }
-  })
-  return exists;
+    let exists = false;
+    arr.forEach(v => {
+        if (value.match(v)) {
+            exists = true;
+        }
+    })
+    return exists;
 }
 
 export default {
-  resolveUrl: (url: string) => {
-    return url;
-  },
-  convertWebStyle: (style) => {
-    style = {...style};
-    if (style.paddingLeft) {
-      style.paddingLeft = Taro.pxTransform(style.paddingLeft);
-    }
-    if (style.paddingRight) {
-      style.paddingRight = Taro.pxTransform(style.paddingRight);
-    }
-    if (style.paddingBottom) {
-      style.paddingBottom = Taro.pxTransform(style.paddingBottom);
-    }
-    if (style.paddingTop) {
-      style.paddingTop = Taro.pxTransform(style.paddingTop);
-    }
+    resolveUrl: (url: string) => {
+        return url;
+    },
+    convertWebStyle: (style) => {
+        style = {...style};
+        if (style.paddingLeft) {
+            style.paddingLeft = Taro.pxTransform(style.paddingLeft);
+        }
+        if (style.paddingRight) {
+            style.paddingRight = Taro.pxTransform(style.paddingRight);
+        }
+        if (style.paddingBottom) {
+            style.paddingBottom = Taro.pxTransform(style.paddingBottom);
+        }
+        if (style.paddingTop) {
+            style.paddingTop = Taro.pxTransform(style.paddingTop);
+        }
 
 
-    if (style.marginLeft) {
-      style.marginLeft = Taro.pxTransform(style.marginLeft);
-    }
-    if (style.marginRight) {
-      style.marginRight = Taro.pxTransform(style.marginRight);
-    }
-    if (style.marginBottom) {
-      style.marginBottom = Taro.pxTransform(style.marginBottom);
-    }
-    if (style.marginTop) {
-      style.marginTop = Taro.pxTransform(style.marginTop);
-    }
+        if (style.marginLeft) {
+            style.marginLeft = Taro.pxTransform(style.marginLeft);
+        }
+        if (style.marginRight) {
+            style.marginRight = Taro.pxTransform(style.marginRight);
+        }
+        if (style.marginBottom) {
+            style.marginBottom = Taro.pxTransform(style.marginBottom);
+        }
+        if (style.marginTop) {
+            style.marginTop = Taro.pxTransform(style.marginTop);
+        }
 
-    if (style.fontSize) {
-      style.fontSize = Taro.pxTransform(style.fontSize);
-    }
-    if (style.borderRadius && !'%'.match(style.borderRadius)) {
-      style.borderRadius = Taro.pxTransform(style.borderRadius);
-    }
+        if (style.fontSize) {
+            style.fontSize = Taro.pxTransform(style.fontSize);
+        }
+        if (style.borderRadius && !'%'.match(style.borderRadius)) {
+            style.borderRadius = Taro.pxTransform(style.borderRadius);
+        }
 
-    if (style.width && !'%'.match(style.width)) {
-      style.width = Taro.pxTransform(style.width);
-    }
-    if (style.height && !'%'.match(style.height)) {
-      style.height = Taro.pxTransform(style.height);
-    }
+        if (style.width && !'%'.match(style.width)) {
+            style.width = Taro.pxTransform(style.width);
+        }
+        if (style.height && !'%'.match(style.height)) {
+            style.height = Taro.pxTransform(style.height);
+        }
 
-    return style;
-  },
-  gotoLink: (link) => {
-    if (!indexOf([], link)) {
-      Taro.navigateTo({url: link}).then();
-    } else {
-      Taro.switchTab({url: link}).then();
-    }
-  },
-  indexOf,
-  compareVersion: (v1, v2) => {
-    v1 = v1.split('.')
-    v2 = v2.split('.')
-    const len = Math.max(v1.length, v2.length)
+        return style;
+    },
+    gotoLink: (link) => {
+        if (!indexOf([], link)) {
+            Taro.navigateTo({url: link}).then();
+        } else {
+            Taro.switchTab({url: link}).then();
+        }
+    },
+    indexOf,
+    compareVersion: (v1, v2) => {
+        v1 = v1.split('.')
+        v2 = v2.split('.')
+        const len = Math.max(v1.length, v2.length)
 
-    while (v1.length < len) {
-      v1.push('0')
-    }
-    while (v2.length < len) {
-      v2.push('0')
-    }
+        while (v1.length < len) {
+            v1.push('0')
+        }
+        while (v2.length < len) {
+            v2.push('0')
+        }
 
-    for (let i = 0; i < len; i++) {
-      const num1 = parseInt(v1[i])
-      const num2 = parseInt(v2[i])
+        for (let i = 0; i < len; i++) {
+            const num1 = parseInt(v1[i])
+            const num2 = parseInt(v2[i])
 
-      if (num1 > num2) {
-        return 1
-      } else if (num1 < num2) {
-        return -1
-      }
+            if (num1 > num2) {
+                return 1
+            } else if (num1 < num2) {
+                return -1
+            }
+        }
+
+        return 0
+    },
+    showLoading: () => {
+        Taro.showLoading({title: '加载中'}).then();
+    },
+    hideLoading() {
+        Taro.hideLoading();
     }
-
-    return 0
-  },
 }
