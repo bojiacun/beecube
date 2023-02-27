791f8ff38b72a64670699b448c2dfb2f2ef6aeea
18612365082 <bojinhong@126.com>
2023-02-27 19:58:41 +0800

    完善框架

---
+++ wxapp/src/components/pagelist/index.tsx
@@ -0,0 +1,12 @@
+
+export interface PageViewListTabItem {
+    label: string;
+    id: string|number;
+    selected?: boolean;
+}
+
+const PageViewList = () => {
+    return (<></>);
+}
+
+export default PageViewList;
