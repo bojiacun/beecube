8bc68ed40be2a5466aca12443a7ca035270e6476
18612365082 <bojinhong@126.com>
2023-02-28 18:53:20 +0800

    完善框架

--- wxapp/src/components/goods/GoodsItemView.tsx
+++ wxapp/src/components/goods/GoodsItemView.tsx
@@ -1,10 +1,15 @@
 import {View} from "@tarojs/components";
+import FallbackImage from "../FallbackImage";
 
+
+export interface GoodsItemViewProps extends Partial<any> {
+
+}
 
 const GoodsItemView = () => {
     return (
-        <View>
-
+        <View className={'bg-white rounded-lg shadow-lg overflow-hidden'}>
+            <FallbackImage className={'block'} mode={'widthFix'} />
         </View>
     );
 }
