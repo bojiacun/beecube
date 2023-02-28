3cc5bac72167121c7ed8f33785dbf7dae941e919
18612365082 <bojinhong@126.com>
2023-02-28 14:41:28 +0800

    完善框架

--- wxapp/src/pages/my/wallet.tsx
+++ wxapp/src/pages/my/wallet.tsx
@@ -4,6 +4,7 @@ import {Navigator, Text, View} from "@tarojs/components";
 import LoginView from "../../components/login";
 import {connect} from "react-redux";
 import ListView, {ListViewTabItem} from "../../components/listview";
+import request from "../../lib/request";
 
 // @ts-ignore
 @connect((state: any) => (
@@ -12,25 +13,53 @@ import ListView, {ListViewTabItem} from "../../components/listview";
     }
 ))
 export default class Index extends Component<any, any> {
+    template: any;
+    tabs: ListViewTabItem[];
+
     constructor(props) {
         super(props);
         this.handleWithdraw = this.handleWithdraw.bind(this);
         this.loadData = this.loadData.bind(this);
-
+        this.template = (data: any) => {
+            let money = data.money + '';
+            return (
+                <View className={'bg-white px-4 py-2'}>
+                    <View className={'flex justify-between'}>
+                        <Text>{data.descripiton}</Text>
+                        <View className={'text-gray-800'}>
+                            <Text className={'text-lg'}>{money.split('.')[0]}</Text>
+                            <Text className={'text-sm'}>.{money.split(',')[1]}</Text>
+                        </View>
+                    </View>
+                    <View className={'text-sm text-gray-300'}>{data.createTime}</View>
+                </View>
+            )
+        };
+        this.tabs = [
+            { label: '全部', id: '', template: this.template },
+            { label: '支出', id: '1', template: this.template },
+            { label: '收入', id: '2', template: this.template },
+            { label: '充值', id: '3', template: this.template },
+            { label: '提现', id: '4', template: this.template }
+        ];
     }
 
-    tabs: [
-        {label: '全部', id: ''}, {label: '支出', id: '1'}, {label: '收入', id: '2'},{label: '充值', id: '3'},{label: '提现', id: '4'}
-    ]
+    types: {
+        1: '支出',
+        2: "收入",
+        3: '充值',
+        4: '提现'
+    }
 
 
-    loadData(pageIndex: number, tab: ListViewTabItem, index: number) {
-
+    loadData(pageIndex: number, tab: ListViewTabItem) {
+        return request.get('/app/api/members/money/records', {params: {page: pageIndex, type: tab.id}});
     }
 
     handleWithdraw() {
 
     }
+
     render() {
         const {context} = this.props;
         const {userInfo} = context;
@@ -44,13 +73,14 @@ export default class Index extends Component<any, any> {
                         <View><Text className={'text-xl'}>{money.split('.')[0]}</Text><Text className={'text-sm'}>.{money.split('.')[0]}</Text></View>
                         <View className={'flex space-x-2'}>
                             <Navigator className={'bg-white text-indigo rounded-full text-indigo-500 py-1 px-4'}>充值</Navigator>
-                            <Text onClick={this.handleWithdraw} className={'text-white border-solid border-1 border-white bg-indigo-400 py-1 px-4 rounded-full'}>提现</Text>
+                            <Text onClick={this.handleWithdraw}
+                                  className={'text-white border-solid border-1 border-white bg-indigo-400 py-1 px-4 rounded-full'}>提现</Text>
                         </View>
                     </View>
 
                     <ListView
                         tabs={this.tabs}
-                        dataFetcher={}
+                        dataFetcher={this.loadData}
                     />
                 </LoginView>
             </PageLayout>
