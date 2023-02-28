90e49e4bc93b3e50c76320af5601e9e36e2bd79a
18612365082 <bojinhong@126.com>
2023-02-28 14:21:47 +0800

    完善框架

--- wxapp/src/components/listview/index.tsx
+++ wxapp/src/components/listview/index.tsx
@@ -1,42 +1,66 @@
-import {FC, useState} from "react";
+import {FC, ReactElement, useState} from "react";
 import styles from './index.module.scss';
 import 'react-tabs/style/react-tabs.css';
 import {Text, View} from "@tarojs/components";
 import classNames from "classnames";
-import {usePullDownRefresh} from "@tarojs/taro";
+import {usePullDownRefresh, useReachBottom} from "@tarojs/taro";
 
 export interface ListViewTabItem {
     label: string;
-    id: string|number;
+    id: string | number;
     selected?: boolean;
+    template: (data: any) => ReactElement;
 }
 
-export interface ListViewProps extends Partial<any>{
+export interface ListViewProps extends Partial<any> {
     tabs: ListViewTabItem[];
     onTabChanged?: Function;
     dataFetcher: (pageIndex: number, tab: ListViewTabItem, index: number) => Promise<any>;
 }
 
-const ListView : FC<ListViewProps> = (props) => {
-    const {children, tabs, onTabChanged = ()=>{}, dataFetcher} = props;
+const ListView: FC<ListViewProps> = (props) => {
+    const {
+        tabs, onTabChanged = () => {
+        }, dataFetcher
+    } = props;
+    const initDatas = tabs.map(() => []);
     const [selectedIndex, setSelectedIndex] = useState<number>(0);
-    const [datas, setDatas] = useState<any[]>([]);
+    const [datas, setDatas] = useState<any[]>(initDatas);
     const [page, setPage] = useState<number>(1);
 
 
-    usePullDownRefresh(()=>{});
+    //下拉刷新
+    usePullDownRefresh(() => {
+        dataFetcher(1, tabs[selectedIndex], selectedIndex).then(res => {
+            datas[selectedIndex] = res.data.result;
+            setDatas([...datas]);
+        });
+        setPage(1);
+    });
+
+    //下拉加载
+    useReachBottom(() => {
+        dataFetcher(page + 1, tabs[selectedIndex], selectedIndex).then(res => {
+            datas[selectedIndex].pushAll(res.data.result);
+            setDatas([...datas]);
+        })
+        setPage(page + 1);
+    });
+
 
     return (
         <>
-            <View className={'bg-white px-4 py-3 flex items-center justify-between space-x-2 text-gray-700'} style={{overflowY: 'hidden', overflowX: 'auto'}}>
+            <View className={'bg-white px-4 py-3 flex items-center justify-between space-x-2 text-gray-700'}
+                  style={{overflowY: 'hidden', overflowX: 'auto'}}>
                 {tabs.map((tab, index) => {
                     return (
-                        <Text className={classNames(index === selectedIndex ? styles.active:'')} onClick={()=>{
+                        <Text className={classNames(index === selectedIndex ? styles.active : '')} onClick={() => {
                             setSelectedIndex(index);
                             onTabChanged(tab, index);
                             setPage(1);
-                            dataFetcher(1,tab, index).then(res=>{
-                                setDatas(res.data.result);
+                            dataFetcher(1, tab, index).then(res => {
+                                datas[index] = res.data.result;
+                                setDatas([...datas]);
                             });
                         }}>
                             {tab.label}
@@ -44,7 +68,21 @@ const ListView : FC<ListViewProps> = (props) => {
                     );
                 })}
             </View>
-
+            {tabs.map((tab, index) => {
+                let data = datas[index];
+                if (data.length == 0) {
+                    return (
+                        <View className={'text-center text-3xl mt-12'}>
+                            <Text className={'iconfont icon-zanwushuju'}/>
+                        </View>
+                    );
+                }
+                return (
+                    <View>
+                        {tab.template(data)}
+                    </View>
+                );
+            })}
         </>
     );
 }
