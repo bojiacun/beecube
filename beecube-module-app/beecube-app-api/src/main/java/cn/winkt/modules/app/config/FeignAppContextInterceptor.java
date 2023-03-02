195f646218f018f86d88d768d31891cc1e27aad6
18612365082 <bojinhong@126.com>
2023-03-02 15:03:21 +0800

    完善框架

---
+++ beecube-module-app/beecube-app-api/src/main/java/cn/winkt/modules/app/config/FeignAppContextInterceptor.java
@@ -0,0 +1,15 @@
+package cn.winkt.modules.app.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.jeecg.common.constant.CommonConstant;
+import org.jeecg.config.AppContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FeignAppContextInterceptor implements RequestInterceptor {
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        requestTemplate.header(CommonConstant.X_APP_ID, AppContext.getApp());
+    }
+}
