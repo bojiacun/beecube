753c6e98d37ec21148c6e8e16e0bfc29fc6fdd23
18612365082 <bojinhong@126.com>
2023-03-04 17:47:32 +0800

    完善框架

--- wxapp/src/components/diy/modules/GoodsList/index.tsx
+++ wxapp/src/components/diy/modules/GoodsList/index.tsx
@@ -1,16 +1,41 @@
 import { View } from "@tarojs/components";
+import {useEffect, useState} from "react";
+import request from "../../../../lib/request";
+import AuctionGoodsItem from "../../../goods/AuctionGoodsItem";
 
 
 
 const GoodsListModule = (props: any) => {
     const { index, basic, style, ...rest } = props;
+    const layoutStyle = parseInt(basic.style);
+    const dataSource = parseInt(basic.dataSource);
+    const [goodsList, setGoodsList] = useState<any[]>([]);
 
-    console.log(basic);
-    return (
-        <View {...rest} style={{...style}}>
+    useEffect(()=>{
+        request.get('/paimai/api/goods/list', {params: {type: 1, source: dataSource}}).then(res=>{
+            setGoodsList(res.data.result.records);
+        })
+    }, [dataSource]);
 
-        </View>
-    );
+    if(layoutStyle == 1) {
+        return (
+            <View style={style} className={'grid grid-cols-2 gap-4'} {...rest}>
+                {goodsList.map((item:any)=>{
+                    return (<AuctionGoodsItem data={item} />);
+                })}
+            </View>
+        );
+    }
+    else if(layoutStyle == 2) {
+        return (
+            <View {...rest} style={{...style}}>
+                {goodsList.map((item:any)=>{
+                    return (<AuctionGoodsItem data={item} />);
+                })}
+            </View>
+        );
+    }
+    return <></>;
 }
 
 export default GoodsListModule;
