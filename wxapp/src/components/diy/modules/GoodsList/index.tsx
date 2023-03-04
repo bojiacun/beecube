84f44f6dcaae2110f66bd2d2a811df4087828911
18612365082 <bojinhong@126.com>
2023-03-04 21:38:15 +0800

    完善框架

--- wxapp/src/components/diy/modules/GoodsList/index.tsx
+++ wxapp/src/components/diy/modules/GoodsList/index.tsx
@@ -1,7 +1,8 @@
-import { View } from "@tarojs/components";
+import {Navigator, View, Text} from "@tarojs/components";
 import {useEffect, useState} from "react";
 import request from "../../../../lib/request";
-import AuctionGoodsItem from "../../../goods/AuctionGoodsItem";
+import FallbackImage from "../../../FallbackImage";
+import Taro from "@tarojs/taro";
 
 
 
@@ -21,16 +22,36 @@ const GoodsListModule = (props: any) => {
         return (
             <View style={style} className={'grid grid-cols-2 gap-4'} {...rest}>
                 {goodsList.map((item:any)=>{
-                    return (<AuctionGoodsItem data={item} />);
+                    return (
+                        <View className={'bg-white overflow-hidden'} style={{borderRadius: Taro.pxTransform(parseFloat(basic.itemBorderRadius))}}>
+                            <Navigator url={'/pages/goods/detail?id='+item.id}>
+                                <View className={'relative'} style={{width: '100%', paddingTop: '100%'}}>
+                                    <FallbackImage mode={'aspectFit'} className={'absolute z-0 inset-0 block w-full h-full'} src={item.images.split(',')[0]}/>
+                                </View>
+                                <View className={'px-2 mt-2'}>{item.title}</View>
+                                <View className={'px-2 mb-2 text-sm'}>
+                                    起拍价 <Text className={'text-red-500'}>RMB</Text> <Text className={'text-red-500 text-base'}>{item.startPrice}</Text>
+                                </View>
+                            </Navigator>
+                        </View>
+                    );
                 })}
             </View>
         );
     }
     else if(layoutStyle == 2) {
         return (
-            <View {...rest} style={{...style}}>
+            <View className={'flex space-x-4 flex-nowrap'} style={{...style, overflowX: 'auto', overflowY: 'hidden'}} {...rest}>
                 {goodsList.map((item:any)=>{
-                    return (<AuctionGoodsItem data={item} />);
+                    return (
+                        <View style={{borderRadius: Taro.pxTransform(parseFloat(basic.itemBorderRadius))}}>
+                            <Navigator url={'/pages/goods/detail?id='+item.id}>
+                                <FallbackImage mode={'widthFix'} className={'rounded block w-full'} src={item.images.split(',')[0]}/>
+                                <View className={'px-2 mt-2'}>{item.title}</View>
+                                <View className={'px-2 mb-2'}>RMB {item.startPrice}</View>
+                            </Navigator>
+                        </View>
+                    );
                 })}
             </View>
         );
