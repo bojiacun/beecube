0c3182cfe84406948717b351cdb151e724a9c97f
18612365082 <bojinhong@126.com>
2023-03-04 22:29:16 +0800

    完善框架

--- adminui/app/components/page-designer/modules/paimai/AsyncPerformanceList.tsx
+++ adminui/app/components/page-designer/modules/paimai/AsyncPerformanceList.tsx
@@ -1,8 +1,8 @@
-import { AttributeTabs, registerModule } from "../../component"
+import {AttributeTabs, registerModule} from "../../component"
 import image from 'assets/designer/s4_2.png';
 import biaoqian from 'assets/designer/biaoqian.png';
-import React, { useEffect, useState } from "react";
-import BoxSettings, { DEFAULT_BOX_STYLES } from "../BoxSettings";
+import React, {useEffect, useState} from "react";
+import BoxSettings, {DEFAULT_BOX_STYLES} from "../BoxSettings";
 import {getPagedGoods, getPagedPerformance, getShopClasses} from "./service";
 import {resolveUrl} from "~/utils/utils";
 import {Form, Formik} from "formik";
@@ -18,7 +18,6 @@ export const ASYNC_PERFORMANCE_LIST_MODULE = "ASYNC_PERFORMANCE_LIST_MODULE";
 export const defaultData = {
     basic: {
         itemBorderRadius: 15,
-        style: 1,
         count: 2,
         dataSource: 1,
     },
@@ -29,47 +28,46 @@ export const defaultData = {
 };
 
 const AsyncPerformanceListModuleAttribute = (props: any) => {
-    const { onUpdate, data } = props;
-    let _data = { ...defaultData, ...data };
+    const {onUpdate, data} = props;
+    let _data = {...defaultData, ...data};
 
-    const handleOnSubmit1 = (values:any) => {
+    const handleOnSubmit1 = (values: any) => {
         _data.basic = values;
         onUpdate({..._data});
     }
-    const handleOnSubmit2 = (values:any) => {
+    const handleOnSubmit2 = (values: any) => {
         _data.style = values;
         onUpdate({..._data});
     }
 
     return (
         <AttributeTabs tabs={['控件设置', '样式设置']}>
-            <div style={{ padding: 15 }}>
+            <div style={{padding: 15}}>
                 <Formik initialValues={_data.basic} onSubmit={handleOnSubmit1}>
                     {
                         (formik) => {
                             return (
-                                <Form method={'post'} onChange={(e)=>formik.submitForm()}>
+                                <Form method={'post'} onChange={(e) => formik.submitForm()}>
                                     <BootstrapRadioGroup
-                                        options={[{ label: '进行中专场', value: '1' }, { label: '往期专场', value: '2' }]}
+                                        options={[{label: '进行中专场', value: '1'}, {label: '往期专场', value: '2'}]}
                                         name={'dataSource'}
                                         label={'数据类型'}
                                     />
-                                    <BootstrapRadioGroup options={[{ label: '样式一', value: '1' }, { label: '样式二', value: '2' }]} name={'style'} label={'列表样式'} />
-                                    <BootstrapInput label={'数据条数'} name={'count'} />
-                                    <BootstrapInput label={'边框圆角'} name={'itemBorderRadius'} />
+                                    <BootstrapInput label={'数据条数'} name={'count'}/>
+                                    <BootstrapInput label={'边框圆角'} name={'itemBorderRadius'}/>
                                 </Form>
                             );
                         }
                     }
                 </Formik>
             </div>
-            <div style={{ padding: 15 }}>
+            <div style={{padding: 15}}>
                 <Formik initialValues={_data.style} onSubmit={handleOnSubmit2}>
                     {
                         (formik) => {
                             return (
-                                <Form method={'post'} onChange={(e)=>formik.submitForm()}>
-                                    <BoxSettings />
+                                <Form method={'post'} onChange={(e) => formik.submitForm()}>
+                                    <BoxSettings/>
                                 </Form>
                             );
                         }
@@ -81,71 +79,66 @@ const AsyncPerformanceListModuleAttribute = (props: any) => {
 }
 
 const AsyncPerformanceListModule = (props: any) => {
-    const { index, data, isPreview, ...rest } = props;
+    const {index, data, isPreview, ...rest} = props;
     const [goodsList, setAsyncPerformanceList] = useState<any[]>([]);
-    let _data = { ...defaultData, ...data };
+    let _data = {...defaultData, ...data};
     useEffect(() => {
         if (_data.basic.dataSource == 1) {
             //最新商品
-            getPagedPerformance(2,1, _data.basic.count).then(res => {
+            getPagedPerformance(2, 1, _data.basic.count).then(res => {
                 setAsyncPerformanceList(res.data.records);
             });
-        }
-        else if (_data.basic.dataSource == 2) {
-            getPagedGoods(2,2, _data.basic.count).then(res => {
+        } else if (_data.basic.dataSource == 2) {
+            getPagedGoods(2, 2, _data.basic.count).then(res => {
                 setAsyncPerformanceList(res.data.records);
             });
-        }
-        else {
+        } else {
             setAsyncPerformanceList([]);
         }
     }, [data.basic.dataSource, data.basic.count]);
 
     return (
         <div {...rest} style={_data.style}>
-            {_data.basic.style == 1 &&
-                <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
-                    {goodsList.slice(0, _data.basic.count).map((item: any) => {
-                        let itemWidth = '100%';
-                        return (
-                            <div key={item.id} style={{ width: itemWidth, background: 'white', padding: 10, marginBottom: 10, borderRadius: parseInt(_data.basic.itemBorderRadius), position: 'relative' }}>
-                                <div style={{paddingTop: '100%', width: '100%', position: 'relative'}}>
-                                    <img src={resolveUrl(item.preview)} alt={item.name} style={{ position: 'absolute', left: 0, top: 0, display: 'block', width: '100%', height: '100%', objectFit: 'cover', zIndex: 99 }} />
-                                </div>
-                                <h6 style={{ whiteSpace: 'nowrap', overflow: 'hidden', marginBottom: 0, lineHeight: 2 }}>{item.title}</h6>
-                                <div style={{ fontSize: 12, display: 'flex' }}></div>
-                                <div style={{ color: 'gray', fontSize: 12, marginTop: 3 }}>{}</div>
+            <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between'}}>
+                {goodsList.slice(0, _data.basic.count).map((item: any) => {
+                    let itemWidth = '100%';
+                    return (
+                        <div key={item.id} style={{
+                            width: itemWidth,
+                            background: 'white',
+                            padding: 10,
+                            marginBottom: 10,
+                            borderRadius: parseInt(_data.basic.itemBorderRadius),
+                            position: 'relative'
+                        }}>
+                            <div style={{paddingTop: '100%', width: '100%', position: 'relative'}}>
+                                <img src={resolveUrl(item.preview)} alt={item.name} style={{
+                                    position: 'absolute',
+                                    left: 0,
+                                    top: 0,
+                                    display: 'block',
+                                    width: '100%',
+                                    height: '100%',
+                                    objectFit: 'cover',
+                                    zIndex: 99
+                                }}/>
                             </div>
-                        );
-                    })}
-                </div>
-            }
-            {_data.basic.style == 2 &&
-                <div style={{ overflowY: 'hidden', overflowX: 'auto', width: '100%', whiteSpace: 'nowrap' }}>
-                    {goodsList.slice(0, _data.basic.count).map((item: any) => {
-                        let itemWidth = '100%';
-                        return (
-                            <div key={item.id} style={{ width: itemWidth, background: 'white', marginRight: 10, padding: 10, marginBottom: _data.basic.space, borderRadius: parseInt(_data.basic.itemBorderRadius), position: 'relative', display: 'inline-block' }}>
-                                <div style={{paddingTop: '100%', width: '100%', position: 'relative'}}>
-                                    <img src={resolveUrl(item.preview)} alt={item.name} style={{ position: 'absolute', left: 0, top: 0, display: 'block', width: '100%', height: '100%', objectFit: 'cover', zIndex: 99 }} />
-                                </div>
-                                <h6 style={{ whiteSpace: 'nowrap', overflow: 'hidden', marginBottom: 0, lineHeight: 2 }}>{item.title}</h6>
-                                <div style={{ fontSize: 12, display: 'flex' }}></div>
-                                <div style={{ color: 'gray', fontSize: 12, marginTop: 3 }}>{}</div>
-                            </div>
-                        );
-                    })}
-                </div>
-            }
+                            <h6 style={{whiteSpace: 'nowrap', overflow: 'hidden', marginBottom: 0, lineHeight: 2}}>{item.title}</h6>
+                            <div style={{fontSize: 12, display: 'flex'}}></div>
+                            <div style={{color: 'gray', fontSize: 12, marginTop: 3}}>{}</div>
+                        </div>
+                    );
+                })}
+            </div>
+
 
         </div>
     );
 }
 
 
-
-export default function (module='') {
-    if(module === 'paimai') {
+export default function (module = '') {
+    if (module === 'paimai') {
         registerModule(ASYNC_PERFORMANCE_LIST_MODULE, "同步拍专场", image, '拍卖模块', AsyncPerformanceListModule, AsyncPerformanceListModuleAttribute, defaultData);
     }
 }
