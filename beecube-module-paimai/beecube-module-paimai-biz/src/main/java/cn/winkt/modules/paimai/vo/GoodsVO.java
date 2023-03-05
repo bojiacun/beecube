1db27178e9f6ce43aa1a5e3c504480efe8a4cee8
18612365082 <bojinhong@126.com>
2023-03-05 13:07:19 +0800

    完善框架

--- beecube-module-paimai/beecube-module-paimai-biz/src/main/java/cn/winkt/modules/paimai/vo/GoodsVO.java
+++ beecube-module-paimai/beecube-module-paimai-biz/src/main/java/cn/winkt/modules/paimai/vo/GoodsVO.java
@@ -20,6 +20,7 @@ public class GoodsVO {
     private java.lang.Integer delayTime;
     private java.lang.Object commission;
     private java.lang.String performanceId;
+    private String performanceTitle;
     private java.util.Date endTime;
     private java.lang.Object fields;
 
