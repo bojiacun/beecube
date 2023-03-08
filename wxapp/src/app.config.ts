909510c237de1e60cd3170a0c15be33987296e15
18612365082 <bojinhong@126.com>
2023-03-08 13:35:56 +0800

    完善框架

--- wxapp/src/app.config.ts
+++ wxapp/src/app.config.ts
@@ -9,6 +9,7 @@ export default defineAppConfig({
         'pages/goods/confirm',
         'pages/shop/cart',
         'pages/shop/confirm',
+        'pages/shop/list',
         'pages/performance/detail',
         'pages/performance/deadline',
         'pages/performance/public',
