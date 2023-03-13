5358c79a2df6f25831070ee83bb99724646ea5b8
18612365082 <bojinhong@126.com>
2023-03-13 09:09:14 +0800

    完善框架

---
+++ wxapp/src/pages/artciles/detail.tsx
@@ -0,0 +1,42 @@
+import {Component} from "react";
+import PageLayout from "../../layouts/PageLayout";
+import PageLoading from "../../components/pageloading";
+import request from "../../lib/request";
+import {RichText, View} from "@tarojs/components";
+import utils from "../../lib/utils";
+
+
+export default class Index extends Component<any, any> {
+    state: any = {
+        id: null,
+        detail: null,
+    }
+
+    onLoad(options) {
+        request.get('/paimai/api/articles/queryById', {params: {id: options.id}}).then(res=>{
+            this.setState({id: options.id, detail: res.data.result});
+        });
+    }
+
+
+    componentDidMount() {
+    }
+
+
+    render() {
+        const {detail} = this.props;
+        if(detail == null) return <PageLoading />;
+
+        return (
+            <PageLayout statusBarProps={{title: '文章详情'}} style={{backgroundColor: 'white', padding: 16}}>
+                <View className={'text-xl font-bold'}>
+                    {detail.title}
+                </View>
+                <View className={'text-gray-400 mt-2'}>{detail.createTime}</View>
+                <View className={'mt-4'}>
+                    <RichText nodes={utils.resolveHtmlImageWidth(detail.content)} space={'nbsp'}/>
+                </View>
+            </PageLayout>
+        );
+    }
+}
