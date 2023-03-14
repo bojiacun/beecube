e3d72980221c841ac1a9f9d51b74ee09e85bdb00
18612365082 <bojinhong@126.com>
2023-03-15 00:20:50 +0800

    完善框架

--- wxapp/src/pages/articles/normal.tsx
+++ wxapp/src/pages/articles/normal.tsx
@@ -12,20 +12,24 @@ export default class Index extends Component<any, any> {
         list: [],
         loadingMore: false,
         noMore: false,
+        classId: undefined,
+        className: undefined
     }
 
     componentDidMount() {
     }
 
-    loadData(page, clear = false) {
-        return request.get('/paimai/api/articles/list', {
+    loadData(page, clear = false, classId) {
+        request.get('/paimai/api/articles/list', {
             params: {
+                classId: classId,
                 pageNo: page,
                 column: 'createTime',
                 order: 'desc',
-                type: 3,
+                type: 1,
             }
         }).then(res => {
+            utils.hideLoading();
             if (clear) {
                 this.setState({list: res.data.result.records, loadingMore: false, noMore: false});
             } else {
@@ -41,29 +45,31 @@ export default class Index extends Component<any, any> {
     }
 
     onLoad(options: any) {
-        this.setState({options: options})
+        this.setState({classId: options.id});
         utils.showLoading();
-        this.loadData(1, true).then(() => utils.hideLoading());
+        request.get("/paimai/api/articles/classes/detail", {params:{id: options.id}}).then(res=>{
+            this.setState({className: res.data.result?.name});
+        })
+        this.loadData(1, true, options.id);
     }
 
     onReachBottom() {
         this.setState({loadingMore: true, noMore: false});
-        this.loadData(this.state.page + 1, false).then(() => {
-        });
+        this.loadData(this.state.page + 1, false, this.state.classId);
         this.setState({page: this.state.page + 1});
     }
 
     onPullDownRefresh() {
         utils.showLoading();
-        this.loadData(1, true).then(() => utils.hideLoading());
+        this.loadData(1, true, this.state.classId);
         this.setState({page: 1});
     }
 
     render() {
-        const {list, noMore, loadingMore} = this.state;
+        const {list, noMore, loadingMore, className} = this.state;
         return (
             <PageLayout
-                statusBarProps={{title: '服务指南', className: 'border-0 border-b-1 border-gray-200 bg-white border-solid'}}
+                statusBarProps={{title: className, className: 'border-0 border-b-1 border-gray-200 bg-white border-solid'}}
                 enableReachBottom={true}>
                 {list.length == 0 && <NoData/>}
                 <View className={'grid grid-cols-1 px-4 divide-y divide-gray-100'}>
