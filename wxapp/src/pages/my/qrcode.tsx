37ee91231028174fe08449b7e022e5f748ff1ab0
18612365082 <bojinhong@126.com>
2023-03-21 10:22:17 +0800

    完善框架

--- wxapp/src/pages/my/qrcode.tsx
+++ wxapp/src/pages/my/qrcode.tsx
@@ -3,13 +3,31 @@ import Taro from '@tarojs/taro';
 import PageLayout from "../../layouts/PageLayout";
 import {Button, View} from "@tarojs/components";
 import FallbackImage from "../../components/FallbackImage";
-import request from "../../lib/request";
+import request, {API_URL, APP_ID} from "../../lib/request";
+import utils from "../../lib/utils";
 
 export default class Index extends Component<any, any> {
     state:any = {
         qrcode: null
     }
 
+    constructor(props) {
+        super(props);
+        this.handleSaveToPhotoAlbum = this.handleSaveToPhotoAlbum.bind(this);
+    }
+
+
+    handleSaveToPhotoAlbum() {
+        const token = Taro.getStorageSync("TOKEN");
+        Taro.downloadFile({
+            url: API_URL+'/app/api/members/qrcode',
+            header: {'X-Access-Token': token, 'Authorization': token, 'X-App-Id': APP_ID},
+        }).then(res=>{
+            Taro.saveImageToPhotosAlbum({filePath: res.tempFilePath}).then(()=>{
+                utils.showSuccess(false,'保存成功');
+            });
+        })
+    }
 
     componentDidMount() {
         request.get('/app/api/members/qrcode', {responseType: 'arraybuffer'}).then((res:any)=>{
@@ -25,7 +43,7 @@ export default class Index extends Component<any, any> {
                     {this.state.qrcode && <FallbackImage src={'data:image/png;base64,'+this.state.qrcode} className={'w-full block'} mode={'widthFix'} />}
                 </View>
                 <View className={'mt-4 text-center'}>
-                    <Button className={'btn btn-danger'}>保存到相册</Button>
+                    <Button className={'btn btn-danger'} onClick={this.handleSaveToPhotoAlbum}>保存到相册</Button>
                 </View>
             </PageLayout>
         );
