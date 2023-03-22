22034db234ecbcf3126222be59e951570f91b2d8
18612365082 <bojinhong@126.com>
2023-03-22 11:36:33 +0800

    完善框架

--- beecube-module-paimai/beecube-module-paimai-biz/src/main/java/cn/winkt/modules/paimai/controller/wxapp/WxAppOrderController.java
+++ beecube-module-paimai/beecube-module-paimai-biz/src/main/java/cn/winkt/modules/paimai/controller/wxapp/WxAppOrderController.java
@@ -1,14 +1,34 @@
 package cn.winkt.modules.paimai.controller.wxapp;
 
+import cn.winkt.modules.paimai.entity.Goods;
+import cn.winkt.modules.paimai.entity.OrderGoods;
+import cn.winkt.modules.paimai.service.IGoodsService;
+import cn.winkt.modules.paimai.service.IOrderGoodsService;
 import lombok.extern.slf4j.Slf4j;
+import org.jeecg.common.api.vo.Result;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RequestMapping("/paimai/api/orders")
 @RestController
 @Slf4j
 public class WxAppOrderController {
 
+    @Resource
+    IOrderGoodsService orderGoodsService;
 
+    @Resource
+    IGoodsService goodsService;
+
+    @GetMapping("/goods/detail")
+    public Result<?> orderGoodsDetail(@RequestParam String id) {
+        OrderGoods orderGoods = orderGoodsService.getById(id);
+        Goods goods = goodsService.getById(orderGoods.getGoodsId());
+        return Result.OK(goods);
+    }
 
 }
