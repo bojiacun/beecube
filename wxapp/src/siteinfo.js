003fb0821f4b186843fc4f38cc88c586b11d0c7a
18612365082 <bojinhong@126.com>
2023-03-22 19:46:37 +0800

    完善框架

--- wxapp/src/siteinfo.js
+++ wxapp/src/siteinfo.js
@@ -2,7 +2,7 @@
 let baseUrl = "https://api.beecube.winkt.cn";
 module.exports = {
     name: "汇智拍卖",
-    appId: '1635887827756838913',
+    appId: '1635823084673253377',
     version: "1.0.0",
     siteroot: baseUrl,
     copyright: true,
