d85cdce2e156d3618d4b643fffaf7c911315ff1a
18612365082 <bojinhong@126.com>
2023-04-01 00:44:27 +0800

    完善框架

--- beecube-module-app/beecube-app-biz/src/main/java/cn/winkt/modules/app/controller/AppWxOpenEventController.java
+++ beecube-module-app/beecube-app-biz/src/main/java/cn/winkt/modules/app/controller/AppWxOpenEventController.java
@@ -1,6 +1,7 @@
 package cn.winkt.modules.app.controller;
 
 import cn.winkt.modules.app.config.MiniAppOpenService;
+import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.open.api.WxOpenComponentService;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,6 +9,7 @@ import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/app/wxopen/event")
+@Slf4j
 public class AppWxOpenEventController {
 
     @Resource
@@ -16,12 +18,14 @@ public class AppWxOpenEventController {
     @ResponseBody
     @RequestMapping(value = "/authorize", method = {RequestMethod.POST, RequestMethod.PUT})
     public String authorize(@RequestBody String postData) {
+        log.info(postData);
         return "success";
     }
 
     @ResponseBody
     @RequestMapping(value = "/callback/{appId}", method = {RequestMethod.POST, RequestMethod.PUT})
     public String appEventCallback(@RequestBody String postData) {
+        log.info(postData);
         return "success";
     }
 }
