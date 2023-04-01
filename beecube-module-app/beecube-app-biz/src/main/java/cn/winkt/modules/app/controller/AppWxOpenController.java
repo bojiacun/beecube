d311b932cfda6df77abf3640c651d2b009467320
18612365082 <bojinhong@126.com>
2023-04-01 14:45:45 +0800

    完善框架

--- beecube-module-app/beecube-app-biz/src/main/java/cn/winkt/modules/app/controller/AppWxOpenController.java
+++ beecube-module-app/beecube-app-biz/src/main/java/cn/winkt/modules/app/controller/AppWxOpenController.java
@@ -7,6 +7,7 @@ import me.chanjar.weixin.open.api.WxOpenService;
 import org.jeecg.config.JeecgBaseConfig;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -27,6 +28,7 @@ public class AppWxOpenController {
     JeecgBaseConfig jeecgBaseConfig;
 
     @GetMapping("/auth/qrcode")
+    @ResponseBody
     public BufferedImage createPreAuthQrcode() throws WxErrorException, IOException {
         String url = wxOpenService.getWxOpenComponentService().getPreAuthUrl(String.format("%s%s", jeecgBaseConfig.getDomainUrl().getApp(), "/app/wxopen/event/auth"));
         return ImageIO.read(new ByteArrayInputStream(QrcodeUtils.createQrcode(url, null)));
