d5fe74dfca551ec6f749122f3e0741db430e8b52
18612365082 <bojinhong@126.com>
2023-04-01 14:32:24 +0800

    完善框架

--- adminui/app/routes/app/wxopen[.png].tsx
+++ adminui/app/routes/app/wxopen[.png].tsx
@@ -5,7 +5,7 @@ import {API_APP_WXOPEN_AUTHQRCODE, requestWithToken} from "~/utils/request.serve
 export const loader: LoaderFunction = async ({request}) => {
     //获取图片数据
     const qrcodeUrl = API_APP_WXOPEN_AUTHQRCODE + '?_t='+Math.random();
-    const res = await requestWithToken(request)(qrcodeUrl);
+    const res = await fetch(qrcodeUrl);
     console.log(res);
     const result = await res.arrayBuffer();
     return new Response(result, {
