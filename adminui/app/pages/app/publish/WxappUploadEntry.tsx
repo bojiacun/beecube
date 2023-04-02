601cc2720f93d376ab9f7208d7427a95382426a1
18612365082 <bojinhong@126.com>
2023-04-02 11:29:12 +0800

    完善框架

--- adminui/app/pages/app/publish/WxappUploadEntry.tsx
+++ adminui/app/pages/app/publish/WxappUploadEntry.tsx
@@ -7,7 +7,7 @@ export default function WxappUploadEntry(props:any) {
         <>
             {app.authStatus == 'unauthorized' &&
                 <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: 400, height: 400, flexDirection: 'column'}}>
-                    <div><a href={authUrl} className={'btn btn-primary'} target={'_blank'}>点击开始授权</a></div>
+                    <div><a href={authUrl} className={'btn btn-primary'}>点击开始授权</a></div>
                     <Alert show variant={'light'}>
                         点击以上链接，在新页面用微信扫一扫授权发布小程序
                     </Alert>
