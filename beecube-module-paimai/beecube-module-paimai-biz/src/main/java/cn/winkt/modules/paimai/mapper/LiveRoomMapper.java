ac717bdbb8c1cf03dfb5a3dbe0e609ee0ed6d037
18612365082 <bojinhong@126.com>
2023-04-04 00:45:14 +0800

    完善框架

---
+++ beecube-module-paimai/beecube-module-paimai-biz/src/main/java/cn/winkt/modules/paimai/mapper/LiveRoomMapper.java
@@ -0,0 +1,17 @@
+package cn.winkt.modules.paimai.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import cn.winkt.modules.paimai.entity.LiveRoom;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 直播间表
+ * @Author: jeecg-boot
+ * @Date:   2023-04-04
+ * @Version: V1.0
+ */
+public interface LiveRoomMapper extends BaseMapper<LiveRoom> {
+
+}
---
+++ beecube-module-paimai/beecube-module-paimai-biz/src/main/java/cn/winkt/modules/paimai/service/ILiveRoomService.java
@@ -0,0 +1,14 @@
+package cn.winkt.modules.paimai.service;
+
+import cn.winkt.modules.paimai.entity.LiveRoom;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: 直播间表
+ * @Author: jeecg-boot
+ * @Date:   2023-04-04
+ * @Version: V1.0
+ */
+public interface ILiveRoomService extends IService<LiveRoom> {
+
+}
