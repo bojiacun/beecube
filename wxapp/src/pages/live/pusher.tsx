eb7cfd005bd93f5fe46de33e28440784f397c9af
18612365082 <bojinhong@126.com>
2023-04-04 10:21:08 +0800

    完善框架

--- wxapp/src/pages/live/pusher.tsx
+++ wxapp/src/pages/live/pusher.tsx
@@ -1,6 +1,8 @@
 import {Component} from "react";
 import PageLayout from "../../layouts/PageLayout";
 import {connect} from "react-redux";
+import zg from '../../lib/zego';
+import request from "../../lib/request";
 
 
 // @ts-ignore
@@ -13,9 +15,24 @@ import {connect} from "react-redux";
     }
 ))
 export default class Index extends Component<any, any> {
+    roomId: any;
 
+    async componentDidUpdate(prevProps: Readonly<any>) {
+        const {userInfo} = this.props.context;
+        if(!prevProps.userInfo && userInfo && this.roomId) {
+            const result = await request.put('/paimai/api/live/login', userInfo);
+            const token = result.data.result;
+            //登录直播间
+            zg.login(this.roomId, 1, token, (params) => {
+                console.log("登录房间成功", this.roomId, params);
+            },(e) => {
+                console.log("登录房间失败", this.roomId, e);
+            });
+        }
+    }
 
-    onLoad() {
+    async onLoad(options) {
+        this.roomId = options.roomId;
     }
 
 
