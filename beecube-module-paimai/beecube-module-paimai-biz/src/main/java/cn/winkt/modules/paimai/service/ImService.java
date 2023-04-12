d0066a4918624fceb5c25ea3062e4c415184b7f4
18612365082 <bojinhong@126.com>
2023-04-13 07:49:05 +0800

    完善框架

---
+++ beecube-module-paimai/beecube-module-paimai-biz/src/main/java/cn/winkt/modules/paimai/service/ImService.java
@@ -0,0 +1,15 @@
+package cn.winkt.modules.paimai.service;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImService implements ApplicationRunner {
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        //启用服务
+    }
+}
