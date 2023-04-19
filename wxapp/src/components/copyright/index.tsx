0cc58fdc7f9369389f03e8a23a19a3d5d55d8eb7
18612365082 <bojinhong@126.com>
2023-04-19 10:52:09 +0800

    完善框架

--- wxapp/src/components/copyright/index.tsx
+++ wxapp/src/components/copyright/index.tsx
@@ -13,7 +13,7 @@ const Copyright: FC<CopyrightProps> = (props) => {
     return (
         <View className={'flex items-center text-sm mx-4 box-border mt-4'} style={{height: Taro.pxTransform(height)}} {...rest}>
             <View className={'bg-gray-200 flex-1'} style={{height: 1}}></View>
-            <View className={'px-4 text-gray-300'}>{text}</View>
+            <View className={'px-4 text-gray-400'}>{text}</View>
             <View className={'bg-gray-200 flex-1'} style={{height: 1}}></View>
         </View>
     );
