0eeee208aee0a367bfb9f89b6b165aad432d6799
bo <bojinhong@126.com>
2023-04-25 23:50:08 +0800

    项目初始化

--- beecube-module-paimai/wxapp/src/pages/performance/invite.tsx
+++ beecube-module-paimai/wxapp/src/pages/performance/invite.tsx
@@ -1,25 +1,18 @@
 import {Component} from "react";
 import request from "../../lib/request";
 import PageLayout from "../../layouts/PageLayout";
-import {Text, View} from "@tarojs/components";
+import {Navigator, Text, View} from "@tarojs/components";
 import FallbackImage from "../../components/FallbackImage";
 import utils from "../../lib/utils";
+import PageLoading from "../../components/pageloading";
 
 
 export default class Index extends Component<any, any> {
     state: any = {
         id: null,
         detail: null,
-        goodsList: [],
-        loadingMore: false,
-        noMore: false,
-        page: 1,
-        posting: false,
-        status: undefined,
-        message: false,
-        deposited: true,
-        preOffered: false,
     }
+
     onLoad(options) {
         this.setState({id: options.id});
         request.get('/paimai/api/performances/detail', {params: {id: options.id}}).then(res => {
@@ -30,26 +23,32 @@ export default class Index extends Component<any, any> {
 
     render() {
         const {detail} = this.state;
+        if (!detail) return <PageLoading/>;
 
         return (
             <PageLayout statusBarProps={{title: '预约报名'}}>
-                <View className={'p-4 m-4 bg-white rounded-lg shadow-outer space-y-4'}>
-                    <View><FallbackImage src={utils.resolveUrl(detail.preview)} mode={'widthFix'} className={'block w-full box-border'} /></View>
-                    <View className={'flex items-center justify-between'}>
-                        <View className={'text-sm text-red-600 space-x-1'}>{detail.tags && detail.tags.split(',').map(item => <Text
-                            className={'py-1 px-2 border border-1 border-solid border-red-500 rounded'}>{item}</Text>)}</View>
-                        {detail.state == 0 && <View className={'text-gray-400 font-bold'}>未开始</View>}
-                        {detail.state == 1 && <View className={'text-red-600 font-bold'}>进行中</View>}
-                        {detail.state == 2 && <View className={'text-gray-400 font-bold'}>已结束</View>}
-                    </View>
-                    <View className={'font-bold text-lg'}>{detail.title}</View>
-                    <View className={'space-x-4'}>
-                        <Text className={'font-bold'}>拍卖地点</Text>
-                        <Text>{detail.auctionAddress}</Text>
-                    </View>
-                    <View className={'space-x-4'}>
-                        <Text className={'font-bold'}>拍卖时间</Text>
-                        <Text>{detail.auctionTimeRange || detail.startTime}</Text>
+                <View className={'m-4 bg-white rounded-lg shadow-outer overflow-hidden'}>
+                    <FallbackImage src={utils.resolveUrl(detail.preview)} mode={'widthFix'}
+                                   className={'block w-full box-border'}/>
+                    <View className={'p-4 space-y-4'}>
+                        <View className={'text-lg font-bold'}>{detail.title}</View>
+                        <View className={'space-x-4 text-sm text-gray-500'}>
+                            <Text className={''}>拍卖地点</Text>
+                            <Text>{detail.auctionAddress}</Text>
+                        </View>
+                        <View className={'space-x-4 text-gray-500 text-sm'}>
+                            <Text className={''}>拍卖时间</Text>
+                            <Text>{detail.auctionTimeRange || detail.startTime}</Text>
+                        </View>
+                        <View className={'border-t-1 pt-4 border-gray-200'}>
+                            <Navigator className={'flex justify-between items-center'}
+                                       url={'/pages/performance/detail' + (detail.type == 2 ? '2' : '') + '?id=' + detail.id}>
+                                <View className={'title1'}>
+                                    <Text>线上同步拍专场</Text>
+                                </View>
+                                <Text className={'font-bold iconfont icon-youjiantou_huaban text-red-600'}/>
+                            </Navigator>
+                        </View>
                     </View>
                 </View>
             </PageLayout>
