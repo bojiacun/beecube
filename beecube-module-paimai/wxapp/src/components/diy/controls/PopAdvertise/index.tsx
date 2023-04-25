3e6bba7106add5b0d4863418dd62e31b132ab27c
bo <bojinhong@126.com>
2023-04-25 14:56:55 +0800

    项目初始化

--- beecube-module-paimai/wxapp/src/components/diy/controls/PopAdvertise/index.tsx
+++ beecube-module-paimai/wxapp/src/components/diy/controls/PopAdvertise/index.tsx
@@ -2,35 +2,60 @@ import {View, Text, Image} from "@tarojs/components";
 import Taro from "@tarojs/taro";
 import {useEffect, useState} from "react";
 import utils from '../../../../lib/utils';
+import _ from 'lodash';
+import moment from "moment";
 
 
 const PopAdvertise = (props: any) => {
     const { data, basic, style, ...rest } = props;
-    const [hide, setHide] = useState<boolean>(true);
+    const [showItem, setShowItem] = useState<any>();
+    const repeat = parseInt(basic.repeat);
+    const repeatInterval = parseInt(basic.repeatInterval);
 
     useEffect(()=>{
-        let exists = utils.indexOf(Taro.getStorageSync("CLOSED_URLS")||[], basic.link);
-        setHide(exists);
+        //拿到本次要显示的广告图
+        let images = _.sortBy(basic.images, ['sort']);
+        let closedImages = JSON.parse(Taro.getStorageSync("CLOSED_IMAGES") || '[]')||[];
+        let nowDate = moment();
+        let willShowImage = null;
+
+        images.forEach(img=>{
+            if(willShowImage != null) return;
+            let cimg = _.find(closedImages, {url: img.url});
+            if(cimg != null) {
+                let closedDate = moment(cimg.closedAt);
+                //用户已经看过该广告，判断是不是还要显示
+                if (repeat == 1 && closedDate.add(repeatInterval,'hours').isBefore(nowDate)) {
+                    willShowImage = img;
+                }
+            }
+            else {
+                willShowImage = img;
+            }
+        })
+        console.log(willShowImage);
+        setShowItem(willShowImage);
     },[basic]);
 
     const closeAdv = () => {
-        setHide(true);
-        let closedLinks = Taro.getStorageSync("CLOSED_URLS") || [];
-        closedLinks.push(basic.link);
-        Taro.setStorageSync('CLOSED_URLS', closedLinks);
+        showItem.closedAt = moment().format('yyyy-MM-DD HH:mm:ss');
+        let closedLinks = JSON.parse(Taro.getStorageSync("CLOSED_IMAGES")||'[]') || [];
+        closedLinks.push(showItem);
+        Taro.setStorageSync('CLOSED_IMAGES', JSON.stringify(closedLinks));
+        setShowItem(null);
     }
-    if(hide) {
+    if(!showItem) {
         return <></>
     }
 
     const navigateToCouponDetail = () => {
-        Taro.navigateTo({url: basic.link}).then(closeAdv);
+        Taro.navigateTo({url: showItem.url}).then(closeAdv);
     }
 
     return (
         <View {...rest}  style={{width: '100vw',zIndex: 99999, height: '100vh', top: 0, left: 0, position: 'fixed', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', background: `rgba(0,0,0,${style.opacity})`}}>
-            <View style={{width: '50%', position: 'relative', opacity: 1}}>
-                <Image mode={'widthFix'} src={utils.resolveUrl(basic.image)} style={{width: '100%', display: 'block'}} onClick={navigateToCouponDetail} />
+            <View style={{width: '70%', position: 'relative', opacity: 1}}>
+                <Image mode={'widthFix'} src={utils.resolveUrl(showItem.image)} style={{width: '100%', display: 'block'}} onClick={navigateToCouponDetail} />
                 <View style={{marginTop: Taro.pxTransform(20), textAlign: 'center', color: 'white', fontSize: Taro.pxTransform(24), cursor: 'pointer'}}>
                     <Text className="text-white" onClick={closeAdv}>关闭</Text>
                 </View>
