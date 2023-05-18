c8c1e5569b142ab9dc9385d787240d6c2195f2e5
bo <bojinhong@126.com>
2023-05-18 23:14:40 +0800

    项目初始化

--- beecube-module-paimai/wxapp/babel.config.js
+++ beecube-module-paimai/wxapp/babel.config.js
@@ -1,10 +1,31 @@
 // babel-preset-taro 更多选项和默认值：
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
 module.exports = {
-  presets: [
-    ['taro', {
-      framework: 'react',
-      ts: true
-    }]
-  ]
+    presets: [
+        ['taro', {
+            framework: 'react',
+            ts: true
+        }]
+    ],
+    plugins: [
+        [
+            "import",
+            {
+                libraryName: "@taroify/core",
+                libraryDirectory: "",
+                style: true,
+            },
+            "@taroify/core",
+        ],
+        [
+            "import",
+            {
+                libraryName: "@taroify/icons",
+                libraryDirectory: "",
+                camel2DashComponentName: false,
+                style: () => "@taroify/icons/style",
+            },
+            "@taroify/icons",
+        ],
+    ],
 }
