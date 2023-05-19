dbab4de5c5e3b2f0e4c9ca915a29de43e460f016
bo <bojinhong@126.com>
2023-05-19 16:09:05 +0800

    项目初始化

--- adminui/remix.config.js
+++ adminui/remix.config.js
@@ -1,3 +1,6 @@
+const {
+    createRoutesFromFolders,
+} = require("@remix-run/v1-route-convention");
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
     ignoredRouteFiles: ["**/.*"],
@@ -11,5 +14,10 @@ module.exports = {
         v2_meta: true,
         v2_normalizeFormMethod: true,
         v2_routeConvention: true,
+    },
+
+    routes(defineRoutes) {
+        // uses the v1 convention, works in v1.15+ and v2
+        return createRoutesFromFolders(defineRoutes);
     },
 };
