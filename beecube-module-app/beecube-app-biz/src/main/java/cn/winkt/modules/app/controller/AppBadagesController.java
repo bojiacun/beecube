2d901e38ac7a49f2c027eaeace45769db3493535
bo <bojinhong@126.com>
2023-06-06 23:05:02 +0800

    项目初始化

---
+++ beecube-module-app/beecube-app-biz/src/main/java/cn/winkt/modules/app/controller/AppBadagesController.java
@@ -0,0 +1,29 @@
+package cn.winkt.modules.app.controller;
+
+import cn.winkt.modules.app.entity.AppMemberWithdraw;
+import cn.winkt.modules.app.service.IAppMemberWithdrawService;
+import cn.winkt.modules.app.vo.AppBadges;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import org.jeecg.common.api.vo.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/badages")
+public class AppBadagesController {
+
+    @Resource
+    private IAppMemberWithdrawService appMemberWithdrawService;
+
+    @GetMapping
+    public Result<AppBadges> appBadges() {
+        AppBadges appBadges = new AppBadges();
+        LambdaQueryWrapper<AppMemberWithdraw> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(AppMemberWithdraw::getStatus, 0);
+        appBadges.setWithdraws((int) appMemberWithdrawService.count(queryWrapper));
+        return Result.OK(appBadges);
+    }
+}
---
+++ beecube-module-app/beecube-app-biz/src/main/java/cn/winkt/modules/app/vo/AppBadges.java
@@ -0,0 +1,8 @@
+package cn.winkt.modules.app.vo;
+
+import lombok.Data;
+
+@Data
+public class AppBadges {
+    private Integer withdraws;
+}
