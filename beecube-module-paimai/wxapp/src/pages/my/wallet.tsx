ebe067a9239439fbf55434f5bf4922b0fffa1bec
bo <bojinhong@126.com>
2023-07-02 11:19:58 +0800

    项目初始化

--- beecube-module-paimai/wxapp/src/pages/my/wallet.tsx
+++ beecube-module-paimai/wxapp/src/pages/my/wallet.tsx
@@ -5,17 +5,19 @@ import {connect} from "react-redux";
 import ListView, {ListViewTabItem} from "../../components/listview";
 import request from "../../lib/request";
 import Taro from "@tarojs/taro";
+import utils from "../../lib/utils";
 
 const numeral = require('numeral');
 
 // @ts-ignore
 @connect((state: any) => (
     {
-        context: state.context
+        context: state.context,
+        systemInfo: state.context.systemInfo,
     }
 ))
 export default class Index extends Component<any, any> {
-    state:any = {
+    state: any = {
         userInfo: null,
     }
     template: any;
@@ -57,9 +59,9 @@ export default class Index extends Component<any, any> {
     }
 
 
-    componentDidShow(){
+    componentDidShow() {
         //刷新余额
-        request.get('/app/api/members/profile').then(res=>{
+        request.get('/app/api/members/profile').then(res => {
             this.setState({userInfo: res.data.result});
         });
     }
@@ -75,27 +77,40 @@ export default class Index extends Component<any, any> {
 
     render() {
         const {userInfo} = this.state;
+        const {systemInfo} = this.props;
         const money = userInfo?.money ? numeral(userInfo.money).format('0.00') : '0.00';
-
+        const barTop = systemInfo.statusBarHeight;
+        const menuButtonInfo = Taro.getMenuButtonBoundingClientRect();
+        // 获取导航栏高度
+        const barHeight = menuButtonInfo.height + (menuButtonInfo.top - barTop) * 2;
         return (
-            <PageLayout statusBarProps={{title: '我的余额'}}>
-                <View className={'p-4 my-2 mx-4 text-white bg-gradient-to-r from-red-500 to-red-300 rounded space-y-2'}>
-                    <View>余额（元）</View>
-                    <View><Text className={'text-xl'}>{money.split('.')[0]}</Text><Text className={'text-sm'}>.{money.split('.')[1]}</Text></View>
+            <PageLayout showStatusBar={false} style={{background: 'url(https://static.winkt.cn/wallet-bg.png) no-repeat top', backgroundSize: 'contain'}}>
+                <View className={'flex items-center justify-center text-lg relative text-white'} style={{height: barHeight + barTop, paddingTop: barTop}}>
+                    我的钱包
+                    <Text className={'fa fa-chevron-left absolute left-5'} onClick={() => utils.navigateBack()}/>
+                </View>
+                <View className={'p-4 my-2 mx-4 rounded space-y-2'}>
+                    <View>我的余额（元）</View>
+                    <View>
+                        <Text className={'text-sm'}>￥</Text>
+                        <Text className={'text-4xl'}>{money.split('.')[0]}</Text>
+                        <Text className={'text-sm'}>.{money.split('.')[1]}</Text>
+                    </View>
                     <View className={'flex space-x-2'}>
-                        <Navigator url={'/pages/my/wallet/charge'} className={'bg-white text-red rounded-full text-red-500 py-1 px-4'}>充值</Navigator>
+                        <Navigator url={'/pages/my/wallet/charge'} className={'bg-white rounded-full text-yellow-800 text-lg py-2 px-6'}>充值</Navigator>
                         <Text onClick={this.handleWithdraw}
-                              className={'text-white border-solid border-1 border-white bg-red-400 py-1 px-4 rounded-full'}>提现</Text>
+                              className={'text-white bg-yellow-800 py-2 px-6 rounded-full text-lg'}>提现</Text>
                     </View>
                 </View>
-
-                <ListView
-                    tabs={this.tabs}
-                    dataFetcher={this.loadData}
-                    tabStyle={2}
-                    fixed={false}
-                    autoRefresh={true}
-                />
+                <View className={'m-4 rounded-lg bg-white overflow-hidden'}>
+                    <ListView
+                        tabs={this.tabs}
+                        dataFetcher={this.loadData}
+                        tabStyle={2}
+                        fixed={false}
+                        autoRefresh={true}
+                    />
+                </View>
             </PageLayout>
         );
     }
