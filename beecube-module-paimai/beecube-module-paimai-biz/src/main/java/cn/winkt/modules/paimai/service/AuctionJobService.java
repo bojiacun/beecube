5c10d32d00a1aaa2c7db949eb910d6ddb5a6d3c8
bo <bojinhong@126.com>
2023-07-05 13:33:31 +0800

    项目初始化

--- beecube-module-paimai/beecube-module-paimai-biz/src/main/java/cn/winkt/modules/paimai/service/AuctionJobService.java
+++ beecube-module-paimai/beecube-module-paimai-biz/src/main/java/cn/winkt/modules/paimai/service/AuctionJobService.java
@@ -7,26 +7,33 @@ import cn.winkt.modules.app.vo.AppVO;
 import cn.winkt.modules.paimai.config.MiniappServices;
 import cn.winkt.modules.paimai.entity.*;
 import cn.winkt.modules.paimai.service.im.ImClientService;
+import cn.winkt.modules.paimai.vo.GoodsSettings;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.error.WxErrorException;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.jeecg.config.AppContext;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.lang.reflect.InvocationTargetException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class AuctionJobService {
     @Resource
     private IGoodsService goodsService;
-
+    @Resource
+    private WxTemplateMessageService wxTemplateMessageService;
     @Resource
     private IGoodsOfferService goodsOfferService;
 
@@ -61,8 +68,10 @@ public class AuctionJobService {
 
     @Resource
     AuctionHelpService auctionHelpService;
+
     /**
      * 定时任务提醒
+     *
      * @return
      */
     @XxlJob(value = "MESSAGE_NOTIFY")
@@ -73,17 +82,16 @@ public class AuctionJobService {
         apps.forEach(appVO -> {
             log.debug("开始处理App{} 的消息提醒", appVO.getId());
             AppContext.setApp(appVO.getId());
-            try{
+            try {
                 WxMaService wxMaService = miniappServices.getWxMaService(appVO.getId());
                 //获取即将开始的专场提醒
                 Date now = new Date();
                 now = DateUtils.addHours(now, Integer.parseInt(params));
                 notifyPerformance(now, 1, wxMaService);
                 notifyPerformance(now, 2, wxMaService);
-                notifyGoods(now ,1, wxMaService);
+                notifyGoods(now, 1, wxMaService);
                 notifyGoods(now, 2, wxMaService);
-            }
-            catch (Exception exception) {
+            } catch (Exception exception) {
                 log.error(exception.getMessage(), exception);
             }
         });
@@ -102,19 +110,19 @@ public class AuctionJobService {
         for (MessagePool messagePool : messagePools) {
             //发送模板消息
             Goods g = goodsService.getById(messagePool.getGoodsId());
-            if(g == null) continue;
+            if (g == null) continue;
             Performance performance = performanceService.getById(g.getPerformanceId());
             WxMaSubscribeMessage m = new WxMaSubscribeMessage();
             m.setTemplateId(messagePool.getTemplateId());
-            m.setPage("/pages/goods/detail?id="+g.getId());
+            m.setPage("/pages/goods/detail?id=" + g.getId());
             m.setMiniprogramState("formal");
             m.setLang("zh_CN");
             m.setToUser(messagePool.getMemberOpenId());
             List<WxMaSubscribeMessage.MsgData> data = new ArrayList<>();
-            if(type == 1) {
+            if (type == 1) {
                 WxMaSubscribeMessage.MsgData data1 = new WxMaSubscribeMessage.MsgData();
                 data1.setName("thing4");
-                data1.setValue(performance == null ? g.getTitle():performance.getTitle());
+                data1.setValue(performance == null ? g.getTitle() : performance.getTitle());
                 data.add(data1);
 
                 WxMaSubscribeMessage.MsgData data2 = new WxMaSubscribeMessage.MsgData();
@@ -133,11 +141,10 @@ public class AuctionJobService {
                 data4.setName("thing5");
                 data4.setValue(messagePool.getMessage());
                 data.add(data4);
-            }
-            else if(type == 2) {
+            } else if (type == 2) {
                 WxMaSubscribeMessage.MsgData data1 = new WxMaSubscribeMessage.MsgData();
                 data1.setName("thing5");
-                data1.setValue(performance == null ? g.getTitle():performance.getTitle());
+                data1.setValue(performance == null ? g.getTitle() : performance.getTitle());
                 data.add(data1);
 
                 WxMaSubscribeMessage.MsgData data2 = new WxMaSubscribeMessage.MsgData();
@@ -148,7 +155,7 @@ public class AuctionJobService {
 
                 WxMaSubscribeMessage.MsgData data3 = new WxMaSubscribeMessage.MsgData();
                 data3.setName("time7");
-                Date endTime = g.getActualEndTime() == null ? g.getEndTime():g.getActualEndTime();
+                Date endTime = g.getActualEndTime() == null ? g.getEndTime() : g.getActualEndTime();
                 data3.setValue(DateFormatUtils.format(endTime, "yyyy-MM-dd HH:mm:ss"));
                 data.add(data3);
 
@@ -159,10 +166,9 @@ public class AuctionJobService {
                 data.add(data4);
             }
             m.setData(data);
-            try{
+            try {
                 wxMaService.getMsgService().sendSubscribeMsg(m);
-            }
-            catch (Exception exception) {
+            } catch (Exception exception) {
                 log.error(exception.getMessage(), exception);
             }
             messagePool.setStatus(1);
@@ -170,6 +176,7 @@ public class AuctionJobService {
             messagePoolService.updateById(messagePool);
         }
     }
+
     private void notifyPerformance(Date now, Integer type, WxMaService wxMaService) throws WxErrorException {
         //提醒用户啊
         LambdaQueryWrapper<MessagePool> messagePoolLambdaQueryWrapper = new LambdaQueryWrapper<>();
@@ -181,16 +188,16 @@ public class AuctionJobService {
 
         for (MessagePool messagePool : messagePools) {
             Performance p = performanceService.getById(messagePool.getPerformanceId());
-            if(p == null) continue;
+            if (p == null) continue;
             //发送模板消息
             WxMaSubscribeMessage m = new WxMaSubscribeMessage();
             m.setTemplateId(messagePool.getTemplateId());
-            m.setPage("/pages/performance/detail?id="+p.getId());
+            m.setPage("/pages/performance/detail?id=" + p.getId());
             m.setMiniprogramState("formal");
             m.setLang("zh_CN");
             m.setToUser(messagePool.getMemberOpenId());
             List<WxMaSubscribeMessage.MsgData> data = new ArrayList<>();
-            if(type == 1) {
+            if (type == 1) {
                 WxMaSubscribeMessage.MsgData data1 = new WxMaSubscribeMessage.MsgData();
                 data1.setName("thing4");
                 data1.setValue(p.getTitle());
@@ -212,8 +219,7 @@ public class AuctionJobService {
                 data4.setName("thing5");
                 data4.setValue(messagePool.getMessage());
                 data.add(data4);
-            }
-            else if(type == 2) {
+            } else if (type == 2) {
                 WxMaSubscribeMessage.MsgData data1 = new WxMaSubscribeMessage.MsgData();
                 data1.setName("thing5");
                 data1.setValue(p.getTitle());
@@ -237,10 +243,9 @@ public class AuctionJobService {
                 data.add(data4);
             }
             m.setData(data);
-            try{
+            try {
                 wxMaService.getMsgService().sendSubscribeMsg(m);
-            }
-            catch (Exception exception) {
+            } catch (Exception exception) {
                 log.error(exception.getMessage(), exception);
             }
             messagePool.setStatus(1);
@@ -251,6 +256,7 @@ public class AuctionJobService {
 
     /**
      * 定时任务退还保证金
+     *
      * @param params
      * @return
      */
@@ -262,10 +268,9 @@ public class AuctionJobService {
         apps.forEach(appVO -> {
             log.debug("开始处理App {} 的保证金退款", appVO.getId());
             AppContext.setApp(appVO.getId());
-            try{
+            try {
                 auctionHelpService.refundDeposit(appVO.getId(), params);
-            }
-            catch (Exception exception) {
+            } catch (Exception exception) {
                 log.error(exception.getMessage(), exception);
             }
         });
@@ -275,6 +280,7 @@ public class AuctionJobService {
 
     /**
      * 定时任务处理拍卖流程
+     *
      * @param params
      * @return
      */
@@ -286,16 +292,14 @@ public class AuctionJobService {
         apps.forEach(appVO -> {
             log.debug("开始处理App {} 的拍品数据", appVO.getId());
             AppContext.setApp(appVO.getId());
-            try{
+            try {
                 auctionHelpService.resolveGoods();
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 log.error(e.getMessage(), e);
             }
         });
         return ReturnT.SUCCESS;
     }
-
 
 
     /**
@@ -308,6 +312,7 @@ public class AuctionJobService {
         apps.forEach(appVO -> {
             log.debug("处理应用{}未处理订单", appVO.getId());
             AppContext.setApp(appVO.getId());
+            //获取超过5分钟未支付的订单，自动处理并取消订单
             QueryWrapper<GoodsOrder> queryWrapper = new QueryWrapper<>();
             queryWrapper.eq("status", 0);
             queryWrapper.lt("create_time", DateUtils.addMinutes(new Date(), -5));
@@ -315,11 +320,33 @@ public class AuctionJobService {
             goodsOrders.forEach(goodsOrder -> {
                 try {
                     goodsOrderService.cancel(goodsOrder);
-                }
-                catch (Exception exception) {
+                } catch (Exception exception) {
                     log.error(exception.getMessage(), exception);
                 }
             });
+
+            //获取超过2分钟未支付的订单，批量发送模板消息提醒用户去支付
+            GoodsSettings goodsSettings = goodsCommonDescService.queryGoodsSettings();
+            final String templateId = goodsSettings.getOrderNotPayTemplateId();
+            String templateParams = goodsSettings.getOrderNotPayTemplateArgs();
+            if (!StringUtils.isAnyEmpty(templateParams, templateId)) {
+                LambdaQueryWrapper<GoodsOrder> goodsOrderLambdaQueryWrapper = new LambdaQueryWrapper<>();
+                goodsOrderLambdaQueryWrapper.eq(GoodsOrder::getStatus, 0);
+                goodsOrderLambdaQueryWrapper.lt(GoodsOrder::getCreateTime, DateUtils.addMinutes(new Date(), -2));
+                List<GoodsOrder> notPayOrders = goodsOrderService.list(goodsOrderLambdaQueryWrapper);
+                for (GoodsOrder goodsOrder : notPayOrders) {
+                    templateParams = templateParams.replace("{orderId}", goodsOrder.getId());
+                    templateParams = templateParams.replace("{createTime}", DateFormatUtils.format(goodsOrder.getCreateTime(), "yyyy-MM-dd HH:mm:ss"));
+                    templateParams = templateParams.replace("{endPayTime}", DateFormatUtils.format(DateUtils.addMinutes(goodsOrder.getCreateTime(), 5), "yyyy-MM-dd HH:mm:ss"));
+                    templateParams = templateParams.replace("{goodsNames}", goodsOrder.getOrderGoods().stream().map(OrderGoods::getGoodsName).collect(Collectors.joining()));
+                    templateParams = templateParams.replace("{totalPay}", BigDecimal.valueOf(goodsOrder.getPayedPrice()).setScale(2, RoundingMode.CEILING).toString());
+                    try {
+                        wxTemplateMessageService.sendTemplateMessage(templateId, templateParams, "/order/pages/detail?id=" + goodsOrder.getId(), goodsOrder.getMemberId(), AppContext.getApp());
+                    } catch (Exception e) {
+                        log.error(e.getMessage(), e);
+                    }
+                }
+            }
         });
         return ReturnT.SUCCESS;
     }
