8ce6602811b8fcaf0ccd0d4b5ab2559888ad15e7
bo <bojinhong@126.com>
2023-07-07 22:11:37 +0800

    项目初始化

--- adminui/app/routes/paimai/smts/send.tsx
+++ adminui/app/routes/paimai/smts/send.tsx
@@ -5,6 +5,7 @@ import {formData2Json} from "~/utils/utils";
 
 export const action: ActionFunction = async ({request}) => {
     await requireAuthenticated(request);
+    const url = new URL(request.url);
     const formData = await request.formData();
-    return await requestWithToken(request)(API_PAIMAI_SMT_SEND, putFormInit(formData2Json(formData)))
+    return await requestWithToken(request)(API_PAIMAI_SMT_SEND+'?id='+url.searchParams.get('id'), putFormInit(formData2Json(formData)))
 }
