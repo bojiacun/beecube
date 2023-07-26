04b9e263c9b2991dae333e516ba2e0249df85f1c
bo <bojinhong@126.com>
2023-07-26 23:16:51 +0800

    项目初始化

--- adminui/app/routes/reg_success.tsx
+++ adminui/app/routes/reg_success.tsx
@@ -1,6 +1,6 @@
-import {ActionFunction, LinksFunction} from "@remix-run/node";
+import {LinksFunction} from "@remix-run/node";
 import loginPageStyleUrl from "~/styles/page-auth.css";
-import {Alert, Button, Card, Col, Form, Image, InputGroup, NavLink, Row} from "react-bootstrap";
+import {Col, NavLink, Row} from "react-bootstrap";
 import SystemLogo from "~/components/logo";
 
 export const links: LinksFunction = () => {
@@ -19,7 +19,11 @@ const RegisterPage = () => {
                         蜜蜂魔方
                     </h2>
                 </NavLink>
-                <Col md={12} className="d-flex align-items-center px-2 p-lg-5">
+                <Col md={12} className="d-flex flex-column align-items-center justify-content-center px-2 p-lg-5">
+                    <span style={{color: 'green', fontSize: 96}}>
+                        <i className="feather icon-check-circle"></i>
+                    </span>
+                    <h1>注册成功，稍后售前会联系您，请保持手机畅通</h1>
                 </Col>
             </Row>
         </div>
