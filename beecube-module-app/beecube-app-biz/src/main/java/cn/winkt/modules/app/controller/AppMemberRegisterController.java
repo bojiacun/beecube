493a2e939864ae23b35379dec44ed587412ddce8
bo <bojinhong@126.com>
2023-07-27 08:30:32 +0800

    项目初始化

--- beecube-module-app/beecube-app-biz/src/main/java/cn/winkt/modules/app/controller/AppMemberRegisterController.java
+++ beecube-module-app/beecube-app-biz/src/main/java/cn/winkt/modules/app/controller/AppMemberRegisterController.java
@@ -1,5 +1,7 @@
 package cn.winkt.modules.app.controller;
 
+import cn.winkt.modules.app.config.TencentSmsService;
+import cn.winkt.modules.app.config.TencentSmsServices;
 import cn.winkt.modules.app.entity.AppMemberRegister;
 import cn.winkt.modules.app.service.IAppMemberRegisterService;
 import cn.winkt.modules.app.service.IAppSettingService;
@@ -10,13 +12,16 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jeecg.common.api.vo.Result;
 import org.jeecg.common.aspect.annotation.AutoDict;
 import org.jeecg.common.aspect.annotation.AutoLog;
+import org.jeecg.common.exception.JeecgBootException;
 import org.jeecg.common.system.base.controller.JeecgController;
 import org.jeecg.common.system.query.QueryGenerator;
 import org.jeecg.common.util.DateUtils;
+import org.jeecg.common.util.RedisUtil;
 import org.jeecg.config.AppContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +34,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Map;
 
 /**
 * @Description: 应用会员表
@@ -43,10 +49,37 @@ import java.util.Arrays;
 public class AppMemberRegisterController extends JeecgController<AppMemberRegister, IAppMemberRegisterService> {
    @Autowired
    private IAppMemberRegisterService appMemberRegisterService;
+    @Resource
+    TencentSmsServices tencentSmsServices;
 
-   @Resource
-   private IAppSettingService appSettingService;
+    @Resource
+    RedisUtil redisUtil;
+    @PostMapping("/send")
+    public Result<Boolean> sendCode(@RequestBody Map<String, String> postData) {
+        TencentSmsService service = tencentSmsServices.getService(AppContext.getApp());
+        if(service == null) {
+            throw new JeecgBootException("获取短信服务失败");
+        }
+        String mobile = postData.get("mobile");
+        String vcode = RandomStringUtils.randomNumeric(6);
+        service.sendCode(mobile, vcode);
+        redisUtil.set("MOBILE_CODE:"+mobile, vcode);
+        redisUtil.expire("MOBILE_CODE:"+mobile, 300);
+        log.debug("发送验证码为 {} {}", mobile, vcode);
+        return Result.OK(true);
+    }
 
+    @PutMapping("/check")
+    public Result<Boolean> checkCode(@RequestBody Map<String, String> postData) {
+        String mobile = postData.get("mobile");
+        String code = postData.get("code");
+        String vcode = (String)redisUtil.get("MOBILE_CODE:"+mobile);
+        if(StringUtils.isNotEmpty(vcode) && vcode.equals(code)) {
+            redisUtil.del("MOBILE_CODE:"+mobile);
+            return Result.OK(true);
+        }
+        return Result.OK(false);
+    }
    /**
     * 分页列表查询
     *
