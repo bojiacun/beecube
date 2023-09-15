f75a9f85066640ff2384a8ae0735bfe6bc79dd53
bo <bojinhong@126.com>
2023-09-15 09:29:17 +0800

    项目初始化

--- beecube-module-app/beecube-app-biz/src/main/java/cn/winkt/modules/app/controller/AppPublishController.java
+++ beecube-module-app/beecube-app-biz/src/main/java/cn/winkt/modules/app/controller/AppPublishController.java
@@ -197,7 +197,7 @@ public class AppPublishController extends JeecgController<AppPublish, IAppPublis
             } else if (result.getStatus() == 0) {
                 publish.setStatus(2);
             }
-            log.debug("微信返回的审核状态为 {}", JSONObject.toJSONString(result));
+            log.info("微信返回的审核状态为 {}", JSONObject.toJSONString(result));
             appPublishService.updateById(publish);
         }
 
