25d8b1f9c9ccc8220c7c558c2d5cfb7a036f6a24
bo <bojinhong@126.com>
2024-09-09 17:44:48 +0800

    修复已知BUG

--- adminui/app/components/tinymce-editor/index.tsx
+++ adminui/app/components/tinymce-editor/index.tsx
@@ -21,7 +21,7 @@ const TinymceEditor: React.FC<TinymceEditorProps> = (props) => {
     }
     return (
         <Editor
-            apiKey={"8x16kx5cbqpuv0ppcc9yamh9biaokrtmfa76tmlpn8ydxx6f"}
+            apiKey={"prl21nffzhk03ervc04sm5u81oqb0ej1ciubjsqo6j8k82t9"}
             initialValue={formik.values[name]}
             onBlur={handleEditorChange}
             textareaName={name}
